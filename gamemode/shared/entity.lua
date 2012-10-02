96475655b0e1f346988740b9c84359b231a3ea4a
FPtje <fpeijnenburg@gmail.com>
2012-10-02 18:12:20 +0200

    cleaned up doorlooking

--- gamemode/shared/entity.lua
+++ gamemode/shared/entity.lua
@@ -85,16 +85,14 @@ end
  ---------------------------------------------------------*/
 if CLIENT then
 	function meta:DrawOwnableInfo()
+		if LocalPlayer():InVehicle() then return end
+
 		local pos = {x = ScrW()/2, y = ScrH() / 2}
 
 		local ownerstr = ""
 
-		if ValidEntity(self:GetDoorOwner()) and self:GetDoorOwner().Nick then
-			ownerstr = self:GetDoorOwner():Nick() .. "\n"
-		end
-
 		for k,v in pairs(player.GetAll()) do
-			if self:OwnedBy(v) and v ~= self:GetDoorOwner() then
+			if self:OwnedBy(v) then
 				ownerstr = ownerstr .. v:Nick() .. "\n"
 			end
 		end
@@ -111,143 +109,57 @@ if CLIENT then
 			ownerstr = ownerstr .. string.format(LANGUAGE.keys_other_allowed)..Player(self.DoorData.AllowedToOwn):Nick().."\n"
 		end
 
-		if not LocalPlayer():InVehicle() then
-			local blocked = self.DoorData.NonOwnable
-			local st = nil
-			local whiteText = false -- false for red, true for white text
+		self.DoorData.title = self.DoorData.title or ""
 
-			self.DoorData.title = self.DoorData.title or ""
+		local blocked = self.DoorData.NonOwnable
+		local st = self.DoorData.title .. "\n"
+		local superadmin = LocalPlayer():IsSuperAdmin()
+		local whiteText = true -- false for red, true for white text
 
-			if self:IsOwned() then
-				whiteText = true
-				if superAdmin then
-					if blocked then
-						st = self.DoorData.title .. "\n"..LANGUAGE.keys_allow_ownership
-					else
-						if ownerstr == "" then
-							st = self.DoorData.title .. "\n"..LANGUAGE.keys_disallow_ownership .. "\n"
-						else
-							if self:OwnedBy(LocalPlayer()) and not self.DoorData.GroupOwn and not self.DoorData.TeamOwn then
-								st = self.DoorData.title .. "\n".. LANGUAGE.keys_owned_by .."\n" .. ownerstr
-							elseif not self.DoorData.GroupOwn and not self.DoorData.TeamOwn then
-								st = self.DoorData.title .. "\n".. LANGUAGE.keys_owned_by .."\n" .. ownerstr .. LANGUAGE.keys_disallow_ownership .. "\n"
-							elseif not self:IsVehicle() then
-								local grouptext = ""
-								if self.DoorData.TeamOwn then
-									for k, v in pairs( self.DoorData.TeamOwn ) do
-										if v then
-											grouptext = grouptext .. RPExtraTeams[k].name .. "\n"
-										end
-									end
-								else
-									grouptext = self.DoorData.GroupOwn .. "\n"
-								end
-								st = self.DoorData.title .. "\n" .. grouptext .. LANGUAGE.keys_disallow_ownership .. "\n"
-							end
-						end
-						if self.DoorData.GroupOwn or self.DoorData.TeamOwn and not self:IsVehicle() then
-							st = st .. LANGUAGE.keys_everyone
-						elseif not self:IsVehicle() and self.DoorData.GroupOwn or self.DoorData.TeamOwn then
-							local grouptext = ""
-							if self.DoorData.TeamOwn then
-								for k, v in pairs( self.DoorData.TeamOwn ) do
-									if v then
-										grouptext = grouptext .. RPExtraTeams[k].name .. "\n"
-									end
-								end
-							else
-								grouptext = self.DoorData.GroupOwn .. "\n"
-							end
-							st = st .. grouptext
-						end
-					end
-				else
-					if blocked then
-						st = self.DoorData.title
-					else
-						if ownerstr == "" then
-							st = self.DoorData.title
-						else
-							if self.DoorData.GroupOwn then
-								whiteText = true
-								st = self.DoorData.title .. "\n".. LANGUAGE.keys_owned_by .."\n" .. self.DoorData.GroupOwn
-							elseif self.DoorData.TeamOwn then
-								whiteText = true
-								local grouptext = ""
-								for k, v in pairs( self.DoorData.TeamOwn ) do
-									if v then
-										grouptext = grouptext .. RPExtraTeams[k].name .. "\n"
-									end
-								end
-								st = self.DoorData.title .. "\n".. LANGUAGE.keys_owned_by .."\n" .. grouptext
-							else
-								st = self.DoorData.title .. "\n".. LANGUAGE.keys_owned_by .."\n" .. ownerstr
-							end
-						end
-					end
-				end
-			else
-				if superAdmin then
-					if blocked then
-						whiteText = true
-						st = self.DoorData.title .. "\n".. LANGUAGE.keys_allow_ownership
-					else
-						if self.DoorData.GroupOwn then
-							whiteText = true
-							st = self.DoorData.title .. "\n".. LANGUAGE.keys_owned_by .."\n" .. self.DoorData.GroupOwn
-							if not self:IsVehicle() then
-								st = st .. "\n".. LANGUAGE.keys_everyone
-							end
-						elseif self.DoorData.TeamOwn then
-							whiteText = true
-							local grouptext = ""
-							for k, v in pairs( self.DoorData.TeamOwn ) do
-								if v then
-									grouptext = grouptext .. RPExtraTeams[k].name .. "\n"
-								end
-							end
-							st = self.DoorData.title .. "\n".. LANGUAGE.keys_owned_by .. "\n" .. grouptext
-							if not self:IsVehicle() then
-								st = st .. "\n".. LANGUAGE.keys_everyone
-							end
-						else
-							st = LANGUAGE.keys_unowned.."\n".. LANGUAGE.keys_disallow_ownership
-							if not self:IsVehicle() then
-								st = st .. "\n"..LANGUAGE.keys_cops
-							end
-						end
-					end
-				else
-					if blocked then
-						whiteText = true
-						st = self.DoorData.title
-					else
-						if self.DoorData.GroupOwn then
-							whiteText = true
-							st = self.DoorData.title .. "\n".. LANGUAGE.keys_owned_by .."\n" .. self.DoorData.GroupOwn
-						elseif self.DoorData.TeamOwn then
-							whiteText = true
-							local grouptext = ""
-							for k, v in pairs( self.DoorData.TeamOwn ) do
-								if v then
-									grouptext = grouptext .. RPExtraTeams[k].name .. "\n"
-								end
-							end
-							st = self.DoorData.title .. "\n".. LANGUAGE.keys_owned_by .."\n" .. grouptext
-						else
-							st = LANGUAGE.keys_unowned
-						end
-					end
+		if superadmin and blocked then
+			st = st .. LANGUAGE.keys_allow_ownership .. "\n"
+		end
+
+		if self.DoorData.TeamOwn then
+			st = st .. LANGUAGE.keys_owned_by .."\n"
+
+			for k, v in pairs(self.DoorData.TeamOwn) do
+				if v then
+					st = st .. RPExtraTeams[k].name .. "\n"
 				end
 			end
+		elseif self.DoorData.GroupOwn then
+			st = st .. LANGUAGE.keys_owned_by .."\n"
+			st = st .. self.DoorData.GroupOwn .. "\n"
+		end
 
-			if whiteText then
-				draw.DrawText(st, "TargetID", pos.x + 1, pos.y + 1, Color(0, 0, 0, 200), 1)
-				draw.DrawText(st, "TargetID", pos.x, pos.y, Color(255, 255, 255, 200), 1)
-			else
-				draw.DrawText(st, "TargetID", pos.x , pos.y+1 , Color(0, 0, 0, 255), 1)
-				draw.DrawText(st, "TargetID", pos.x, pos.y, Color(128, 30, 30, 255), 1)
+		if self:IsOwned() then
+			if superAdmin then
+				if ownerstr ~= "" then
+					st = st .. LANGUAGE.keys_owned_by .."\n" .. ownerstr
+				end
+				st = st ..LANGUAGE.keys_disallow_ownership .. "\n"
+			elseif not blocked and ownerstr ~= "" then
+				st = st .. LANGUAGE.keys_owned_by .. "\n" .. ownerstr
 			end
+		elseif not blocked then
+			if superAdmin then
+				st = LANGUAGE.keys_unowned .."\n".. LANGUAGE.keys_disallow_ownership
+				if not self:IsVehicle() then
+					st = st .. "\n"..LANGUAGE.keys_cops
+				end
+			elseif not self.DoorData.GroupOwn and not self.DoorData.TeamOwn then
+				whiteText = false
+				st = LANGUAGE.keys_unowned
+			end
+		end
+
+		if whiteText then
+			draw.DrawText(st, "TargetID", pos.x + 1, pos.y + 1, Color(0, 0, 0, 200), 1)
+			draw.DrawText(st, "TargetID", pos.x, pos.y, Color(255, 255, 255, 200), 1)
+		else
+			draw.DrawText(st, "TargetID", pos.x , pos.y+1 , Color(0, 0, 0, 255), 1)
+			draw.DrawText(st, "TargetID", pos.x, pos.y, Color(128, 30, 30, 255), 1)
 		end
 	end
 
@@ -334,10 +246,14 @@ local function SetDoorGroupOwnable(ply, arg)
 	ent.DoorData.TeamOwn = nil
 	ent.DoorData.GroupOwn = arg
 
-	if arg == "" then ent.DoorData.GroupOwn = nil ent.DoorData.TeamOwn = nil end
+	if arg == "" then
+		ent.DoorData.GroupOwn = nil
+		ent.DoorData.TeamOwn = nil
+	end
 
 	-- Save it for future map loads
 	DB.SetDoorGroup(ent, arg)
+	DB.StoreTeamDoorOwnability(ent)
 
 	ply.LookingAtDoor = nil
 
