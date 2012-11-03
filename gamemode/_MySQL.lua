4ef4d4006d5cce9a24f8bbdc59529e27f682df30
FPtje <fpeijnenburg@gmail.com>
2012-11-03 18:37:09 +0100

    Updated README in _MySQL.lua.

--- gamemode/_MySQL.lua
+++ gamemode/_MySQL.lua
@@ -19,19 +19,8 @@ RP_MySQLConfig.Database_port = 3306 -- This is the port of the MySQL server. Aga
 --[[
 MANUAL!
 HOW TO USE MySQL FOR DARKRP!
-Download andyvincent's gm_MySQL OO module to your hard drive:
-http://www.facepunch.com/showthread.php?t=933647
-
-Unpack it
-
-on the SERVER:
-	- Put libmySQL.dll in the same folder as HL2.exe OR srcds.exe (e.g srcds\orangebox OR steamapps\username\garrysmod)
-	- Put gmsv_mysqloo.dll in garrysmod\lua\includes\modules\. If any of those folders don't exist, create them.
-		You can also copy and paste the lua folder from the download to garrysmod/
-	- Make sure the configurations in this file (_MySQL.lua) are set up correctly
-	- TIP: Make this file read-only so it doesn't get changed in an SVN update!
-		But if you get problems with the MySQL database, remove this file, update from SVN and set it up again, things might change in the SVN!
-
+Download andyvincent's/Drakehawke's gm_MySQL OO module and read the guide here:
+http://www.facepunch.com/showthread.php?t=1220537
 
 
 WHAT TO DO IF YOU CAN'T GET IT TO WORK!
@@ -40,5 +29,5 @@ WHAT TO DO IF YOU CAN'T GET IT TO WORK!
 		the logs are at garrysmod/data/DarkRP_logs/ on the SERVER!
 		The MySQL lines in the log always precede with "MySQL Error:" (without the quotation marks)
 	- make sure the settings in this file (_MySQL.lua) are correct
-	- make sure the MySQL server is accessible from the outside
+	- make sure the MySQL server is accessible from the outside world
 ]]
