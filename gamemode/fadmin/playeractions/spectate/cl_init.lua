daf102e77eb559a91f03108f53b252043c909239
FPtje <fpeijnenburg@gmail.com>
2012-12-15 12:17:37 +0100

    [Add] Spectate free roaming: MUCH BETTER THAN NOCLIPPING WHILE CLOAKED. FADMIN SPECTATE IS UNDETECTABLE, CLOAKING ISN'T!
    Please know that Spectaing is the best way to identify minges!
    
    Please test and report bugs.

--- gamemode/fadmin/playeractions/spectate/cl_init.lua
+++ gamemode/fadmin/playeractions/spectate/cl_init.lua
@@ -1,3 +1,9 @@
+local stopSpectating, startFreeRoam
+local specEnt
+local thirdperson = false
+local isRoaming = false
+local roamPos = Vector(0) -- the position when roaming free
+
 /*---------------------------------------------------------------------------
 startHooks
 FAdmin tab buttons
@@ -17,41 +23,66 @@ FAdmin.StartHooks["zzSpectate"] = function()
 	end)
 end
 
-local specEnt
-local stopSpectating
-local thirdperson = false
+/*---------------------------------------------------------------------------
+Get the thirdperson position
+---------------------------------------------------------------------------*/
+local function getThirdPersonPos(ply)
+	local aimvector = LocalPlayer():GetAimVector()
+	local startPos = ply:GetShootPos()
+	local endpos = startPos - aimvector * 100
+
+	local tracer = {
+		start = startPos,
+		endpos = endpos,
+		filter = specEnt
+	}
+
+	local trace = util.TraceLine(tracer)
+
+	return trace.HitPos + trace.HitNormal * 10
+end
+
+/*---------------------------------------------------------------------------
+Get the CalcView table
+---------------------------------------------------------------------------*/
+local view = {
+	vm_origin = Vector(0, 0, -13000)
+}
+local function getCalcView()
+	if not isRoaming then
+		if thirdperson then
+			view.origin = getThirdPersonPos(specEnt)
+			view.angles = LocalPlayer():EyeAngles()
+		else
+			view.origin = specEnt:GetShootPos()
+			view.angles = specEnt:EyeAngles()
+		end
+
+		return view
+	end
+
+	view.origin = roamPos
+	view.angles = LocalPlayer():EyeAngles()
+
+	return view
+end
 
 /*---------------------------------------------------------------------------
 specCalcView
 Override the view for the player to look through the spectated player's eyes
 ---------------------------------------------------------------------------*/
 local function specCalcView(ply, origin, angles, fov)
-	if not IsValid(specEnt) then
-		stopSpectating()
+	if not IsValid(specEnt) and not isRoaming then
+		startFreeRoam()
 		return
 	end
 
-	local view = {
-		origin = specEnt:GetShootPos(),
-		vm_origin = Vector(0,0,-13000),
-		angles = specEnt:EyeAngles()
-	}
+	local view = getCalcView()
 
-	if thirdperson then
-		local aimvector = LocalPlayer():GetAimVector()
-		local endpos = view.origin - aimvector * 100
-
-		local tracer = {start = view.origin,
-		endpos = endpos,
-		filter = specEnt}
-
-		local trace = util.TraceLine(tracer)
-
-		view.origin = trace.HitPos + trace.HitNormal * 10
-		view.angles = LocalPlayer():EyeAngles()
+	if IsValid(specEnt) then
+		specEnt:SetNoDraw(not thirdperson)
 	end
 
-	specEnt:SetNoDraw(not thirdperson)
 	return view
 end
 
@@ -66,22 +97,106 @@ local function specBinds(ply, bind, pressed)
 	elseif bind == "+attack" and pressed then
 		thirdperson = not thirdperson
 		return true
+	elseif bind == "+attack2" and pressed then
+		if not isRoaming then
+			startFreeRoam()
+		else
+			local trace = util.QuickTrace(roamPos, LocalPlayer():GetAimVector() * 25000)
+			if IsValid(trace.Entity) and trace.Entity:IsPlayer() then
+				RunConsoleCommand("FAdmin", "Spectate", trace.Entity:UserID())
+			end
+		end
+
+		return true
 	end
-	-- Do not return true otherwise, spectating admins should be able to move to avoid getting detected
+	-- Do not return otherwise, spectating admins should be able to move to avoid getting detected
+end
+
+/*---------------------------------------------------------------------------
+Spectate think
+Free roaming position updates
+---------------------------------------------------------------------------*/
+local function specThink()
+	local ply = LocalPlayer()
+
+	if not isRoaming or ply:KeyDown(IN_USE) then return end
+
+	local roamSpeed = 15
+	local aimVec = ply:GetAimVector()
+	local direction = Vector(0)
+
+	if ply:KeyDown(IN_FORWARD) then
+		direction = aimVec
+	elseif ply:KeyDown(IN_BACK) then
+		direction = -aimVec
+	end
+
+	if ply:KeyDown(IN_MOVELEFT) then
+		direction = direction - aimVec:Angle():Right()
+	elseif ply:KeyDown(IN_MOVERIGHT) then
+		direction = direction + aimVec:Angle():Right()
+	end
+
+	if ply:KeyDown(IN_SPEED) then
+		roamSpeed = 30
+	elseif ply:KeyDown(IN_WALK) then
+		roamSpeed = 5
+	end
+
+	direction:Normalize()
+
+	roamPos = roamPos + direction * roamSpeed
+end
+
+/*---------------------------------------------------------------------------
+Draw help on the screen
+---------------------------------------------------------------------------*/
+local function drawHelp()
+	draw.WordBox(2, 10, ScrH() / 2, "Left mouse: toggle thirdperson", "UiBold", Color(0,0,0,120), Color(255, 255, 255, 255))
+	draw.WordBox(2, 10, ScrH() / 2 + 20, "Right mouse: (Un)select player to spectate", "UiBold", Color(0,0,0,120), Color(255, 255, 255, 255))
+	draw.WordBox(2, 10, ScrH() / 2 + 40, "Jump: Stop spectating", "UiBold", Color(0,0,0,120), Color(255, 255, 255, 255))
+end
+
+/*---------------------------------------------------------------------------
+Start roaming free, rather than spectating a given player
+---------------------------------------------------------------------------*/
+startFreeRoam = function()
+	if IsValid(specEnt) then
+		roamPos = thirdperson and getThirdPersonPos(specEnt) or specEnt:GetShootPos()
+		specEnt:SetNoDraw(false)
+	else
+		roamPos = LocalPlayer():GetShootPos()
+	end
+
+	specEnt = nil
+	isRoaming = true
 end
 
 /*---------------------------------------------------------------------------
 specEnt
 Spectate a player
 ---------------------------------------------------------------------------*/
-local function spectatePlayer(um)
+local function startSpectate(um)
+	isRoaming = um:ReadBool()
 	specEnt = um:ReadEntity()
+
+	if isRoaming then
+		startFreeRoam()
+	end
 
 	hook.Add("CalcView", "FAdminSpectate", specCalcView)
 	hook.Add("PlayerBindPress", "FAdminSpectate", specBinds)
-	hook.Add("ShouldDrawLocalPlayer", "FAdminSpectate", function() return thirdperson end)
+	hook.Add("ShouldDrawLocalPlayer", "FAdminSpectate", function() return isRoaming or thirdperson end)
+	hook.Add("Think", "FAdminSpectate", specThink)
+	hook.Add("HUDPaint", "FAdminSpectate", drawHelp)
+
+	timer.Create("FAdminSpectatePosUpdate", 0.5, 0, function()
+		if not isRoaming then return end
+
+		RunConsoleCommand("_FAdmin_SpectatePosUpdate", roamPos.x, roamPos.y, roamPos.z)
+	end)
 end
-usermessage.Hook("FAdminSpectate", spectatePlayer)
+usermessage.Hook("FAdminSpectate", startSpectate)
 
 /*---------------------------------------------------------------------------
 stopSpectating
@@ -91,6 +206,10 @@ stopSpectating = function()
 	hook.Remove("CalcView", "FAdminSpectate")
 	hook.Remove("PlayerBindPress", "FAdminSpectate")
 	hook.Remove("ShouldDrawLocalPlayer", "FAdminSpectate")
+	hook.Remove("Think", "FAdminSpectate")
+	hook.Remove("HUDPaint", "FAdminSpectate")
+
+	timer.Destroy("FAdminSpectatePosUpdate")
 
 	if IsValid(specEnt) then
 		specEnt:SetNoDraw(false)
--- gamemode/fadmin/playeractions/spectate/sv_init.lua
+++ gamemode/fadmin/playeractions/spectate/sv_init.lua
@@ -1,36 +1,57 @@
 local function Spectate(ply, cmd, args)
 	if not FAdmin.Access.PlayerHasPrivilege(ply, "Spectate") then FAdmin.Messages.SendMessage(ply, 5, "No access!") return end
-	if not args[1] then return end
 
 	local target = FAdmin.FindPlayer(args[1])
 	target = target and target[1] or nil
-	if not IsValid(target) or target == ply then return end
+	target = IsValid(target) and target ~= ply and target or nil
 
-	ply.FAdminSpectating = target
+	ply.FAdminSpectatingEnt = target
+	ply.FAdminSpectating = true
 
 	umsg.Start("FAdminSpectate", ply)
-		umsg.Entity(ply.FAdminSpectating)
+		umsg.Bool(target == nil) -- Is the player roaming?
+		umsg.Entity(ply.FAdminSpectatingEnt)
 	umsg.End()
 
-	FAdmin.Messages.SendMessage(ply, 4, "You are now spectating "..target:Nick() .. " ("..target:SteamID()..")")
+
+	local targetText = IsValid(target) and (target:Nick() .. " ("..target:SteamID()..")") or ""
+	FAdmin.Messages.SendMessage(ply, 4, "You are now spectating "..targetText)
 end
 
 local function SpectateVisibility(ply, viewEnt)
-	if IsValid(ply.FAdminSpectating) then
-		AddOriginToPVS(ply.FAdminSpectating:GetShootPos())
+	if not ply.FAdminSpectating then return end
+
+	if IsValid(ply.FAdminSpectatingEnt) then
+		AddOriginToPVS(ply.FAdminSpectatingEnt:GetShootPos())
+	end
+
+	if ply.FAdminSpectatePos then
+		AddOriginToPVS(ply.FAdminSpectatePos)
 	end
 end
 hook.Add("SetupPlayerVisibility", "FAdminSpectate", SpectateVisibility)
 
+local function setSpectatePos(ply, cmd, args)
+	if not FAdmin.Access.PlayerHasPrivilege(ply, "Spectate") then FAdmin.Messages.SendMessage(ply, 5, "No access!") return end
+
+	if not ply.FAdminSpectating or not args[3] then return end
+	local x, y, z = tonumber(args[1] or 0), tonumber(args[2] or 0), tonumber(args[3] or 0)
+
+	ply.FAdminSpectatePos = Vector(x, y, z)
+end
+concommand.Add("_FAdmin_SpectatePosUpdate", setSpectatePos)
+
 local function endSpectate(ply, cmd, args)
+	ply.FAdminSpectatingEnt = nil
 	ply.FAdminSpectating = nil
+	ply.FAdminSpectatePos = nil
 end
 concommand.Add("_FAdmin_StopSpectating", endSpectate)
 
 local function playerVoice(listener, talker)
 	local canhear, surround = GAMEMODE:PlayerCanHearPlayersVoice(listener, talker)
 	for k,v in pairs(player.GetAll()) do
-		if (v.FAdminSpectating == listener and canhear) or v.FAdminSpectating == talker then
+		if (v.FAdminSpectatingEnt == listener and canhear) or v.FAdminSpectatingEnt == talker then
 			return true, surround
 		end
 	end
