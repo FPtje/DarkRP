564240bf5eb841955e1126ed50c6703838a844a8
FPtje <fpeijnenburg@gmail.com>
2013-03-29 17:59:05 +0100

    Different way of cleaning map. Fixes #455

--- gamemode/fadmin/cleanup/sv_init.lua
+++ gamemode/fadmin/cleanup/sv_init.lua
@@ -1,6 +1,6 @@
 local function ClearDecals(ply, cmd, args)
 	if not FAdmin.Access.PlayerHasPrivilege(ply, "CleanUp") then FAdmin.Messages.SendMessage(ply, 5, "No access!") return end
-	
+
 	for k,v in pairs(player.GetAll()) do
 		v:ConCommand("r_cleardecals")
 	end
@@ -12,14 +12,23 @@ local function StopSounds(ply, cmd, args)
 
 	umsg.Start("FAdmin_StopSounds")
 	umsg.End()
-	
+
 	FAdmin.Messages.ActionMessage(ply, player.GetAll(), "You have stopped all sounds", "All sounds have been stopped", "Stopped all sounds")
 end
 
 local function CleanUp(ply, cmd, args)
 	if not FAdmin.Access.PlayerHasPrivilege(ply, "CleanUp") then FAdmin.Messages.SendMessage(ply, 5, "No access!") return end
-	RunConsoleCommand("gmod_admin_cleanup") // This way if DarkRPs installed it will use the over-ride that fixes clean-up breaking stuff.
-	
+
+	local List = cleanup.GetList()
+	for k, ply in pairs(List) do
+		for _, cleanupType in pairs(ply) do
+			for _, ent in pairs(cleanupType) do
+				if not IsValid(ent) then continue end
+				ent:Remove()
+			end
+		end
+		List[k] = nil
+	end
 	FAdmin.Messages.ActionMessage(ply, player.GetAll(), "You have cleaned up the map", "The map has been cleaned up", "Cleaned up the map")
 end
 
@@ -27,6 +36,6 @@ FAdmin.StartHooks["CleanUp"] = function()
 	FAdmin.Commands.AddCommand("ClearDecals", ClearDecals)
 	FAdmin.Commands.AddCommand("StopSounds", StopSounds)
 	FAdmin.Commands.AddCommand("CleanUp", CleanUp)
-	
+
 	FAdmin.Access.AddPrivilege("CleanUp", 2)
 end
