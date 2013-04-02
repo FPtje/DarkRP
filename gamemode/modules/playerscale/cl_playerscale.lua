6ce5722803ce10d76dd607ceb2a1414fa2e1b5f5
FPtje <fpeijnenburg@gmail.com>
2013-04-02 13:15:48 +0200

    Player scaling!

---
+++ gamemode/modules/playerscale/cl_playerscale.lua
@@ -0,0 +1,8 @@
+local function doScale(um)
+	local ply = um:ReadEntity()
+	local scale = um:ReadFloat()
+
+	ply:SetModelScale(scale, 1)
+	ply:SetHull(Vector(-16, -16, 0), Vector(16, 16, 64 * scale))
+end
+usermessage.Hook("darkrp_playerscale", doScale)
