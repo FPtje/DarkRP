0a21bba189e3348ec9904ae5632af17c41a9ab60
FPtje <fpeijnenburg@gmail.com>
2013-04-08 13:02:49 +0200

    Font thing. Attempt at fixing #484

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -7,22 +7,6 @@ util.PrecacheSound("earthquake.mp3")
 CUR = "$"
 
 /*---------------------------------------------------------------------------
-Fonts
----------------------------------------------------------------------------*/
-surface.CreateFont ("DarkRPHUD1", {
-	size = 16,
-	weight = 600,
-	antialias = true,
-	shadow = true,
-	font = "DejaVu Sans"})
-surface.CreateFont ("DarkRPHUD2", {
-	size = 23,
-	weight = 400,
-	antialias = true,
-	shadow = false,
-	font = "coolvetica"})
-
-/*---------------------------------------------------------------------------
 Names
 ---------------------------------------------------------------------------*/
 -- Make sure the client sees the RP name where they expect to see the name
@@ -564,82 +548,100 @@ FAdmin.StartHooks["DarkRP"] = function()
 	function(ply) return FAdmin.Access.PlayerHasPrivilege(LocalPlayer(), "rp_commands", ply) end, teamban)
 end
 
--- These fonts used to exist in GMod 12 but were removed in 13.
-surface.CreateFont("Trebuchet18", {
-	size = 18,
-	weight = 500,
-	antialias = true,
-	shadow = false,
-	font = "Trebuchet MS"})
-surface.CreateFont("Trebuchet19", {
-	size = 19,
-	weight = 500,
-	antialias = true,
-	shadow = false,
-	font = "Trebuchet MS"})
-surface.CreateFont("Trebuchet20", {
-	size = 20,
-	weight = 500,
-	antialias = true,
-	shadow = false,
-	font = "Trebuchet MS"})
-surface.CreateFont("Trebuchet22", {
-	size = 22,
-	weight = 500,
-	antialias = true,
-	shadow = false,
-	font = "Trebuchet MS"})
-surface.CreateFont("Trebuchet24", {
-	size = 24,
-	weight = 500,
-	antialias = true,
-	shadow = false,
-	font = "Trebuchet MS"})
-surface.CreateFont("TabLarge", {
-	size = 17,
-	weight = 700,
-	antialias = true,
-	shadow = false,
-	font = "Trebuchet MS"})
-surface.CreateFont("UiBold", {
-	size = 16,
-	weight = 800,
-	antialias = true,
-	shadow = false,
-	font = "Default"})
-surface.CreateFont("HUDNumber5", {
-	size = 30,
-	weight = 800,
-	antialias = true,
-	shadow = false,
-	font = "Default"})
-surface.CreateFont("ScoreboardHeader", {
-	size = 32,
-	weight = 500,
-	antialias = true,
-	shadow = false,
-	font = "coolvetica"})
-surface.CreateFont("ScoreboardSubtitle", {
-	size = 22,
-	weight = 500,
-	antialias = true,
-	shadow = false,
-	font = "coolvetica"})
-surface.CreateFont("ScoreboardPlayerName", {
-	size = 19,
-	weight = 500,
-	antialias = true,
-	shadow = false,
-	font = "coolvetica"})
-surface.CreateFont("ScoreboardPlayerName2", {
-	size = 15,
-	weight = 500,
-	antialias = true,
-	shadow = false,
-	font = "coolvetica"})
-surface.CreateFont("ScoreboardPlayerNameBig", {
-	size = 22,
-	weight = 500,
-	antialias = true,
-	shadow = false,
-	font = "coolvetica"})
+/*---------------------------------------------------------------------------
+Fonts
+---------------------------------------------------------------------------*/
+hook.Add("InitPostEntity", "fonts", function()
+	surface.CreateFont ("DarkRPHUD1", {
+		size = 16,
+		weight = 600,
+		antialias = true,
+		shadow = true,
+		font = "DejaVu Sans"})
+	surface.CreateFont ("DarkRPHUD2", {
+		size = 23,
+		weight = 400,
+		antialias = true,
+		shadow = false,
+		font = "coolvetica"})
+
+	-- These fonts used to exist in GMod 12 but were removed in 13.
+	surface.CreateFont("Trebuchet18", {
+		size = 18,
+		weight = 500,
+		antialias = true,
+		shadow = false,
+		font = "Trebuchet MS"})
+	surface.CreateFont("Trebuchet19", {
+		size = 19,
+		weight = 500,
+		antialias = true,
+		shadow = false,
+		font = "Trebuchet MS"})
+	surface.CreateFont("Trebuchet20", {
+		size = 20,
+		weight = 500,
+		antialias = true,
+		shadow = false,
+		font = "Trebuchet MS"})
+	surface.CreateFont("Trebuchet22", {
+		size = 22,
+		weight = 500,
+		antialias = true,
+		shadow = false,
+		font = "Trebuchet MS"})
+	surface.CreateFont("Trebuchet24", {
+		size = 24,
+		weight = 500,
+		antialias = true,
+		shadow = false,
+		font = "Trebuchet MS"})
+	surface.CreateFont("TabLarge", {
+		size = 17,
+		weight = 700,
+		antialias = true,
+		shadow = false,
+		font = "Trebuchet MS"})
+	surface.CreateFont("UiBold", {
+		size = 16,
+		weight = 800,
+		antialias = true,
+		shadow = false,
+		font = "Default"})
+	surface.CreateFont("HUDNumber5", {
+		size = 30,
+		weight = 800,
+		antialias = true,
+		shadow = false,
+		font = "Default"})
+	surface.CreateFont("ScoreboardHeader", {
+		size = 32,
+		weight = 500,
+		antialias = true,
+		shadow = false,
+		font = "coolvetica"})
+	surface.CreateFont("ScoreboardSubtitle", {
+		size = 22,
+		weight = 500,
+		antialias = true,
+		shadow = false,
+		font = "coolvetica"})
+	surface.CreateFont("ScoreboardPlayerName", {
+		size = 19,
+		weight = 500,
+		antialias = true,
+		shadow = false,
+		font = "coolvetica"})
+	surface.CreateFont("ScoreboardPlayerName2", {
+		size = 15,
+		weight = 500,
+		antialias = true,
+		shadow = false,
+		font = "coolvetica"})
+	surface.CreateFont("ScoreboardPlayerNameBig", {
+		size = 22,
+		weight = 500,
+		antialias = true,
+		shadow = false,
+		font = "coolvetica"})
+end)
