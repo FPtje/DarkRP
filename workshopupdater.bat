aa70500fde5c53b9211e87f1400f03b4c324cebe
FPtje <fpeijnenburg@gmail.com>
2013-06-23 16:54:56 +0200

    Workshopper

--- workshopupdater.bat
+++ workshopupdater.bat
@@ -1,6 +1,6 @@
 del C:\Users\falco\Documents\Programs\workshop\gma\FPP.gma
-"C:\Users\falco\Documents\Programs\workshop\gmad\gmad.exe" create -out "C:\Users\falco\Documents\Programs\workshop\gma\FPP.gma" -folder "C:\Program Files (x86)\Steam\steamapps\computerabcd\garrysmod\garrysmod\addons\FPP2\
+"../../../bin/gmad.exe" create -out "C:\Users\Falco\Downloads\FPP.gma" -folder "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\Falcos-Prop-protection
 
-"C:\Users\falco\Documents\Programs\workshop\gmpublish" update -addon "C:\Users\falco\Documents\Programs\workshop\gma\FPP.gma" -id "133537219" -changes "Look at this site for the actual changes: https://github.com/FPtje/Falcos-Prop-protection/commits/master"
+"../../../bin/gmpublish" update -addon "C:\Users\Falco\Downloads\FPP.gma" -id "133537219" -changes "Look at this site for the actual changes: https://github.com/FPtje/Falcos-Prop-protection/commits/master"
 
 PAUSE
