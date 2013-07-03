0e5ee8a99885d57fb83c454dd39617975b6c685e
FPtje <fpeijnenburg@gmail.com>
2013-07-03 18:09:39 +0200

    Apparently door_class is still a thing.

--- gamemode/shared/Workarounds.lua
+++ gamemode/shared/Workarounds.lua
@@ -99,3 +99,18 @@ hook.Add("PropBreak", "drp_AntiExploit", function(attacker, ent)
 		constraint.RemoveAll(ent)
 	end
 end)
+
+local allowedDoors = {
+	["prop_dynamic"] = true,
+	["prop_door_rotating"] = true,
+	[""] = true
+}
+
+hook.Add("CanTool", "DoorExploit", function(ply, trace, tool)
+	if not IsValid(ply:GetActiveWeapon()) or not ply:GetActiveWeapon().GetToolObject or not ply:GetActiveWeapon():GetToolObject() then return end
+
+	local tool = ply:GetActiveWeapon():GetToolObject()
+	if not allowedDoors[string.lower(tool:GetClientInfo("door_class") or "")] then
+		return false
+	end
+end)
