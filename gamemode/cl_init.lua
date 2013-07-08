bed63accbbadef8612a917dec1ca8c60f81062f0
FPtje <fpeijnenburg@gmail.com>
2013-07-08 14:33:01 +0200

    Changed version

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,4 +1,4 @@
-GM.Version = "2.4.3"
+GM.Version = "2.5.0"
 GM.Name = "DarkRP"
 GM.Author = "By Rickster, Updated: Pcwizdan, Sibre, philxyz, [GNC] Matt, Chrome Bolt, FPtje Falco, Eusion, Drakehawke"
 
