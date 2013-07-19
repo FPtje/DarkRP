6827fdcba58820ee157402e26fbbc12d6ed95c70
FPtje <fpeijnenburg@gmail.com>
2013-07-19 16:15:43 +0200

    More chat command documentation

---
+++ gamemode/modules/medic/sh_commands.lua
@@ -0,0 +1,11 @@
+local plyMeta = FindMetaTable("Player")
+local getJobTable = fn.Compose{fn.Curry(fn.Flip(fn.GetValue), 2)(RPExtraTeams), plyMeta.Team}
+local isMedic = fn.Compose{fn.Curry(fn.GetValue, 2)("medic"), getJobTable}
+local noMedicExists = fn.Compose{fn.Null, fn.Curry(fn.Filter, 2)(isMedic), player.GetAll}
+
+DarkRP.declareChatCommand{
+	command = "buyhealth",
+	description = "Buy health (only possible if there's no medic!)",
+	delay = 1.5,
+	condition = fn.FOr{isMedic, noMedicExists}
+}
---
+++ gamemode/modules/money/sh_commands.lua
@@ -0,0 +1,29 @@
+DarkRP.declareChatCommand{
+	command = "give",
+	description = "Give money to the player you're looking at.",
+	delay = 1.5
+}
+
+DarkRP.declareChatCommand{
+	command = "dropmoney",
+	description = "Drop money on the floor.",
+	delay = 1.5
+}
+
+DarkRP.declareChatCommand{
+	command = "moneydrop",
+	description = "Drop money on the floor.",
+	delay = 1.5
+}
+
+DarkRP.declareChatCommand{
+	command = "cheque",
+	description = "Write a cheque for a specific person.",
+	delay = 1.5
+}
+
+DarkRP.declareChatCommand{
+	command = "check",
+	description = "Write a cheque for a specific person.",
+	delay = 1.5
+}
