13c2003783f3ae176c4c55073f8a3db54b30d104
FPtje <fpeijnenburg@gmail.com>
2013-07-23 15:32:17 +0200

    Edited DarkRP skin a bit for nicer scrollbars

--- gamemode/modules/f1menu/cl_chatcommandspanel.lua
+++ gamemode/modules/f1menu/cl_chatcommandspanel.lua
@@ -3,6 +3,7 @@ local PANEL = {}
 function PANEL:Init()
 	self:SetBackgroundColor(Color(0, 0, 0, 0))
 	self:EnableVerticalScrollbar()
+	self:SetSpacing(10)
 	self.VBar.Paint = fn.Id
 	self.VBar.btnUp.Paint = fn.Id
 	self.VBar.btnDown.Paint = fn.Id
