0be03790f751629ac9dc6818413e59fa6de83899
FPtje <fpeijnenburg@gmail.com>
2013-08-09 20:21:17 +0200

    Fill data

--- gamemode/modules/f4menu/cl_jobstab.lua
+++ gamemode/modules/f4menu/cl_jobstab.lua
@@ -6,16 +6,17 @@ local PANEL = {}
 function PANEL:Init()
 	self:SetBackgroundColor(Color(0, 0, 0, 0))
 	self:EnableVerticalScrollbar()
-	self:SetSpacing(10)
+	self:SetSpacing(2)
 	self.VBar.Paint = fn.Id
 	self.VBar.btnUp.Paint = fn.Id
 	self.VBar.btnDown.Paint = fn.Id
+
 end
 
--- The white stuff is for testing purposes.
+/*-- The white stuff is for testing purposes.
 function PANEL:Paint(w, h)
 	draw.RoundedBox(0, 0, 0, w, h, Color(255,255,255,255))
-end
+end*/
 
 derma.DefineControl("F4EmptyPanel", "", PANEL, "DPanelList")
 
@@ -31,6 +32,8 @@ function PANEL:Init()
 
 	self.pnlRight = vgui.Create("F4EmptyPanel", self)
 	self.pnlRight:Dock(RIGHT)
+
+	self:fillData()
 end
 
 function PANEL:PerformLayout()
@@ -39,5 +42,13 @@ function PANEL:PerformLayout()
 end
 
 PANEL.Paint = fn.Id
+
+function PANEL:fillData()
+	for i, job in ipairs(RPExtraTeams) do
+		local item = vgui.Create("F4MenuJobButton")
+		item:setDarkRPItem(job)
+		self.pnlLeft:AddItem(item)
+	end
+end
 
 derma.DefineControl("F4MenuJobs", "", PANEL, "DPanel")
