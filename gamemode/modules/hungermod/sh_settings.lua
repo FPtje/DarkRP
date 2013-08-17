b795c4bd0d20ddd34a6e44ca8f20da8cedf1c98d
Bo Anderson <bo@toxicflames.co.uk>
2013-08-17 20:19:53 +0100

    Fixed hunger mod Lua error on load.

--- gamemode/modules/hungermod/sh_settings.lua
+++ gamemode/modules/hungermod/sh_settings.lua
@@ -10,28 +10,30 @@ GM.Config.starverate = 3
 /*---------------------------------------------------------------------------
 Settings for the job and entities
 ---------------------------------------------------------------------------*/
-TEAM_COOK = AddExtraTeam("Cook", {
-	color = Color(238, 99, 99, 255),
-	model = "models/player/mossman.mdl",
-	description = [[As a cook, it is your responsibility to feed the other members
-		of your city.
-		You can spawn a microwave and sell the food you make:
-		/Buymicrowave]],
-	weapons = {},
-	command = "cook",
-	max = 2,
-	salary = 45,
-	admin = 0,
-	vote = false,
-	hasLicense = false,
-	cook = true
-})
+hook.Add("Initialize", "HungerMod_Init", function()
+	TEAM_COOK = AddExtraTeam("Cook", {
+		color = Color(238, 99, 99, 255),
+		model = "models/player/mossman.mdl",
+		description = [[As a cook, it is your responsibility to feed the other members
+			of your city.
+			You can spawn a microwave and sell the food you make:
+			/Buymicrowave]],
+		weapons = {},
+		command = "cook",
+		max = 2,
+		salary = 45,
+		admin = 0,
+		vote = false,
+		hasLicense = false,
+		cook = true
+	})
 
-AddEntity("Microwave", {
-	ent = "microwave",
-	model = "models/props/cs_office/microwave.mdl",
-	price = 400,
-	max = 1,
-	cmd = "/buymicrowave",
-	allowed = TEAM_COOK
-})
+	AddEntity("Microwave", {
+		ent = "microwave",
+		model = "models/props/cs_office/microwave.mdl",
+		price = 400,
+		max = 1,
+		cmd = "/buymicrowave",
+		allowed = TEAM_COOK
+	})
+end)
