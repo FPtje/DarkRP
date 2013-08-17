e012d2a951f5c733ad589a1b1f306aa30ac2ab98
FPtje <fpeijnenburg@gmail.com>
2013-08-17 21:59:20 +0200

    Moved module settings to config.lua

--- gamemode/config/config.lua
+++ gamemode/config/config.lua
@@ -302,3 +302,36 @@ GM.Config.PocketBlacklist = {
 	["money_printer"] = true,
 	["gunlab"] = true,
 }
+
+/*---------------------------------------------------------------------------
+AFK module
+---------------------------------------------------------------------------*/
+GM.Config.afkdemotetime = 600
+
+/*---------------------------------------------------------------------------
+Hitmenu module
+---------------------------------------------------------------------------*/
+-- The minimum price for a hit
+GM.Config.minHitPrice = 200
+-- The minimum distance between a hitman and his customer when they make the deal
+GM.Config.minHitDistance = 150
+-- The text that tells the player he can press use on the hitman to request a hit
+GM.Config.hudText = "I am a hitman.\nPress E on me to request a hit!"
+-- The text above a hitman when he's got a hit
+GM.Config.hitmanText = "Hit\naccepted!"
+-- The cooldown time for a hit target (so they aren't spam killed)
+GM.Config.hitTargetCooldown = 120
+-- How long a customer has to wait to be able to buy another hit (from the moment the hit is accepted)
+GM.Config.hitCustomerCooldown = 240
+
+/*---------------------------------------------------------------------------
+Hungermod module
+---------------------------------------------------------------------------*/
+-- foodspawn - Whether players(non-cooks) can spawn food props or not
+GM.Config.foodspawn = true
+-- foodcost <Amount> - Set food cost
+GM.Config.foodcost = 15
+-- hungerspeed <Amount> - Set the rate at which players will become hungry (2 is the default)
+GM.Config.hungerspeed = 2
+-- starverate <Amount> - How much health that is taken away every second the player is starving  (3 is the default)
+GM.Config.starverate = 3
--- gamemode/modules/hungermod/sh_settings.lua
+++ gamemode/modules/hungermod/sh_settings.lua
@@ -1,12 +1,3 @@
--- foodspawn - Whether players(non-cooks) can spawn food props or not
-GM.Config.foodspawn = true
--- foodcost <Amount> - Set food cost
-GM.Config.foodcost = 15
--- hungerspeed <Amount> - Set the rate at which players will become hungry (2 is the default)
-GM.Config.hungerspeed = 2
--- starverate <Amount> - How much health that is taken away every second the player is starving  (3 is the default)
-GM.Config.starverate = 3
-
 /*---------------------------------------------------------------------------
 Settings for the job and entities
 ---------------------------------------------------------------------------*/
