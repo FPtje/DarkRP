95af7038e5c16e4562746cba161c62019beb9879
FPtje <fpeijnenburg@gmail.com>
2013-09-01 17:08:42 +0200

    Fixed up #999

--- gamemode/modules/deathpov/cl_init.lua
+++ gamemode/modules/deathpov/cl_init.lua
@@ -1,37 +1,36 @@
 hook.Add("CalcView", "rp_deathPOV", function(ply, origin, angles, fov)
-	if GAMEMODE.Config.deathpov and not ply:Alive() then
-		local Ragdoll = ply:GetRagdollEntity()
-		if not IsValid(Ragdoll) then return end
+	if not GAMEMODE.Config.deathpov or ply:Alive() then return end
 
-		if not Ragdoll.BonesRattled then
-			Ragdoll.BonesRattled = true
-			print(Ragdoll)
+	local Ragdoll = ply:GetRagdollEntity()
+	if not IsValid(Ragdoll) then return end
 
-			Ragdoll:InvalidateBoneCache()
-			Ragdoll:SetupBones()
+	if not Ragdoll.BonesRattled then
+		Ragdoll.BonesRattled = true
 
-			local matrix
+		Ragdoll:InvalidateBoneCache()
+		Ragdoll:SetupBones()
 
-			for bone = 0, (Ragdoll:GetBoneCount() or 1) do
-				if Ragdoll:GetBoneName(bone):lower():find("head") then
-					matrix = Ragdoll:GetBoneMatrix(bone)
-					return
-				end
-			end
+		local matrix
 
-			if IsValid(matrix) then
-				matrix:SetScale(Vector(0, 0, 0))
+		for bone = 0, (Ragdoll:GetBoneCount() or 1) do
+			if Ragdoll:GetBoneName(bone):lower():find("head") then
+				matrix = Ragdoll:GetBoneMatrix(bone)
+				return
 			end
 		end
 
-		local head = Ragdoll:LookupAttachment("eyes")
-		head = Ragdoll:GetAttachment(head)
-		if not head or not head.Pos then return end
-
-		return {
-			origin = head.Pos,
-			angles = head.Ang,
-			fov = fov
-		}
+		if IsValid(matrix) then
+			matrix:SetScale(Vector(0, 0, 0))
+		end
 	end
+
+	local head = Ragdoll:LookupAttachment("eyes")
+	head = Ragdoll:GetAttachment(head)
+	if not head or not head.Pos then return end
+
+	return {
+		origin = head.Pos,
+		angles = head.Ang,
+		fov = fov
+	}
 end)
