ae92a62263dc8096aae28578d9bfde5786af0eec
FPtje <fpeijnenburg@gmail.com>
2013-09-07 10:44:45 +0200

    Fixed #1037

--- entities/entities/chatindicator/init.lua
+++ entities/entities/chatindicator/init.lua
@@ -4,7 +4,6 @@ AddCSLuaFile("cl_init.lua")
 include("cl_init.lua")
 
 function ENT:Initialize()
-
 	self:SetModel("models/extras/info_speech.mdl")
 
 	self:SetMoveType(MOVETYPE_NOCLIP)
@@ -13,45 +12,34 @@ function ENT:Initialize()
 end
 
 function ENT:Think()
-	if not IsValid( self.ply ) then -- just in case
-
+	if not IsValid(self.ply) then -- just in case
 		self:Remove()
 
 		return
 	end
 
-	self:SetPos( self.ply:GetPos() + Vector( 0, 0, 85 ) )
-
+	self:SetPos(self.ply:GetPos() + Vector(0, 0, 85))
 end
 
-local function ToggleChatIndicator( ply )
-
-	if not IsValid( ply.ChatIndicator ) then
-
+util.AddNetworkString("DarkRP_ToggleChat")
+local function ToggleChatIndicator(len, ply)
+	if not IsValid(ply.ChatIndicator) then
 		ply.ChatIndicator = ents.Create("chatindicator")
 		ply.ChatIndicator.ply = ply -- plyception
-		ply.ChatIndicator:SetPos( ply:GetPos() + Vector( 0, 0, 85 ) )
+		ply.ChatIndicator:SetPos(ply:GetPos() + Vector(0, 0, 85))
 		ply.ChatIndicator:Spawn()
 		ply.ChatIndicator:Activate()
-
 	else
-
 		ply.ChatIndicator:Remove()
-
 	end
-
 end
-concommand.Add("_DarkRP_ToggleChat", ToggleChatIndicator )
+net.Receive("DarkRP_ToggleChat", ToggleChatIndicator)
 
-local function RemoveChatIndicator( ply )
-
-	if IsValid( ply.ChatIndicator ) then
-
+local function RemoveChatIndicator(ply)
+	if IsValid(ply.ChatIndicator) then
 		ply.ChatIndicator:Remove()
-
 	end
-
 end
-hook.Add("PlayerDisconnected", "Disc_RemoveIndicator", RemoveChatIndicator )
-hook.Add("KeyPress", "Move_RemoveIndicator", RemoveChatIndicator ) -- so people can't abuse the command.
-hook.Add("PlayerDeath", "Die_RemoveIndicator", RemoveChatIndicator )
+hook.Add("PlayerDisconnected", "Disc_RemoveIndicator", RemoveChatIndicator)
+hook.Add("KeyPress", "Move_RemoveIndicator", RemoveChatIndicator) -- so people can't abuse the command.
+hook.Add("PlayerDeath", "Die_RemoveIndicator", RemoveChatIndicator)
