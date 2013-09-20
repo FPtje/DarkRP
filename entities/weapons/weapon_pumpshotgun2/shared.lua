89245da5bbdb49fc615147ecbc3dda48f4c28bdf
FPtje <fpeijnenburg@gmail.com>
2013-09-20 11:49:54 +0200

    Fixed #1065

--- entities/weapons/weapon_pumpshotgun2/shared.lua
+++ entities/weapons/weapon_pumpshotgun2/shared.lua
@@ -56,8 +56,10 @@ function SWEP:Reload()
 		self.Weapon.reloading = true
 		self.Weapon:SetVar("reloadtimer", CurTime() + 0.3)
 		self.Weapon:SendWeaponAnim(ACT_VM_RELOAD)
-		self.Owner:SetAnimation(PLAYER_RELOAD)
 		self:SetIronsights(false)
+		self:NewSetWeaponHoldType(self.HoldType)
+		self.Owner:SetAnimation(PLAYER_RELOAD)
+		self:NewSetWeaponHoldType("normal")
 	end
 end
 
