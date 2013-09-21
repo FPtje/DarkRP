f858cbe4cb4869f125fb30dd60243a34262bf8fa
FPtje <fpeijnenburg@gmail.com>
2013-09-21 23:43:39 +0200

    Added DarkRP MOTD, so I can communicate with users directly.

---
+++ gamemode/modules/darkrpmessages/cl_darkrpmessage.lua
@@ -0,0 +1,28 @@
+local MotdMessage =
+[[
+
+
+---------------------------------------------------------------------------
+			DarkRP Message of the day!
+---------------------------------------------------------------------------
+]]
+
+local endMOTD = "---------------------------------------------------------------------------\n"
+
+local function drawMOTD(text)
+	MsgC(Color(255, 20, 20, 255), MotdMessage)
+	MsgC(Color(255, 255, 255, 255), text)
+	MsgC(Color(255, 20, 20, 255), endMOTD)
+end
+
+local function receiveMOTD(html, len, headers, code)
+	if string.sub(headers.Status, 1, 3) ~= "200" then return end
+	drawMOTD(html)
+end
+
+local function showMOTD()
+	http.Fetch("https://raw.github.com/FPtje/DarkRPMotd/master/motd.txt", receiveMOTD, fn.Id)
+end
+timer.Simple(5, showMOTD)
+
+concommand.Add("DarkRP_motd", showMOTD)
