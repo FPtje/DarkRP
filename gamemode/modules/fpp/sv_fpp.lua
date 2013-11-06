54e09638819653b77734748a3204c5f6979b802e
FPtje <fpeijnenburg@gmail.com>
2013-11-06 20:52:15 +0100

    Fixed https://github.com/FPtje/Falcos-Prop-protection/issues/40

--- gamemode/modules/fpp/sv_fpp.lua
+++ gamemode/modules/fpp/sv_fpp.lua
@@ -1,7 +1,7 @@
 local BlockedModelsExist = sql.QueryValue("SELECT COUNT(*) FROM FPP_BLOCKEDMODELS1;") ~= false
 if not BlockedModelsExist then
 	sql.Query("CREATE TABLE IF NOT EXISTS FPP_BLOCKEDMODELS1(model VARCHAR(140) NOT NULL PRIMARY KEY);")
-	include("pp/FPP_DefaultBlockedModels.lua") -- Load the default blocked models
+	include("sv_defaultblockedmodels.lua") -- Load the default blocked models
 end
 
 AddCSLuaFile("pp/sh_CPPI.lua")
