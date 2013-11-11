2ec05604e748d0c2357107dc3205b61f9f114c32
FPtje <fpeijnenburg@gmail.com>
2013-11-11 20:31:26 +0100

    Fixed #42

--- workshopupdater.bat
+++
@@ -1,6 +0,0 @@
-del C:\Users\falco\Documents\Programs\workshop\gma\FPP.gma
-"../../../bin/gmad.exe" create -out "C:\Users\Falco\Downloads\FPP.gma" -folder "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\Falcos-Prop-protection
-
-"../../../bin/gmpublish" update -addon "C:\Users\Falco\Downloads\FPP.gma" -id "133537219" -changes "Look at this site for the actual changes: https://github.com/FPtje/Falcos-Prop-protection/commits/master"
-
-PAUSE
