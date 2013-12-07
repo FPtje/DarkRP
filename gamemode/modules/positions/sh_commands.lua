0d64887531b2fc165ba1b352485dc3d0d2f1cbc5
FPtje <fpeijnenburg@gmail.com>
2013-12-07 17:02:12 +0100

    Some minor chat command description fixes

--- gamemode/modules/positions/sh_commands.lua
+++ gamemode/modules/positions/sh_commands.lua
@@ -2,7 +2,7 @@ local plyMeta = FindMetaTable("Player")
 
 DarkRP.declareChatCommand{
 	command = "setspawn",
-	description = "reset the spawn position for some job and place a new one at your position (use the command name of the job as argument)",
+	description = "Reset the spawn position for some job and place a new one at your position (use the command name of the job as argument)",
 	delay = 1.5,
 	condition = fn.Curry(fn.Flip(plyMeta.hasDarkRPPrivilege), 2)("rp_commands")
 }
@@ -16,7 +16,7 @@ DarkRP.declareChatCommand{
 
 DarkRP.declareChatCommand{
 	command = "removespawn",
-	description = "Add a spawn position for some job (use the command name of the job as argument)",
+	description = "Remove a spawn position for some job (use the command name of the job as argument)",
 	delay = 1.5,
 	condition = fn.Curry(fn.Flip(plyMeta.hasDarkRPPrivilege), 2)("rp_commands")
 }
