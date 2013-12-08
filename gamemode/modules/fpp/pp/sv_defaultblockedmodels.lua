7f664157aea23d04e3797294df4d6731233ecbcb
FPtje <fpeijnenburg@gmail.com>
2013-12-08 03:51:51 +0100

    capital letter mistake

--- gamemode/modules/fpp/pp/sv_defaultblockedmodels.lua
+++ gamemode/modules/fpp/pp/sv_defaultblockedmodels.lua
@@ -55,6 +55,6 @@ local defaultblocked = {
 
 -- Add to SQLite database
 for k,v in pairs(defaultblocked) do
-	MySQLite.Query("REPLACE INTO FPP_BLOCKEDMODELS1 VALUES("..sql.SQLStr(v)..");")
+	MySQLite.query("REPLACE INTO FPP_BLOCKEDMODELS1 VALUES("..sql.SQLStr(v)..");")
 end
 
