64493997659276841f497534ca23757432ada7e1
Falco Peijnenburg <fpeijnenburg@gmail.com>
2014-01-17 09:28:48 +0100

    Delocalised one instance of MsgC (The others weren't necessary)

--- gamemode/modules/logging/cl_init.lua
+++ gamemode/modules/logging/cl_init.lua
@@ -5,6 +5,6 @@ local function AdminLog(um)
 	local colour = Color(um:ReadShort(), um:ReadShort(), um:ReadShort())
 	local text = um:ReadString() .. "\n"
 	MsgC(Color(255,0,0), "[DarkRP] ")
-	MsgC(colour, text)
+	MsgC(colour, DarkRP.deLocalise(text))
 end
 usermessage.Hook("DRPLogMsg", AdminLog)
