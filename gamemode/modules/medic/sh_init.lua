80bd09dc4f64183c7beb307269cd75d09e3a87c0
Bo Anderson <bo@toxicflames.co.uk>
2014-01-31 09:59:03 +0000

    Dead code removal.

--- gamemode/modules/medic/sh_init.lua
+++ gamemode/modules/medic/sh_init.lua
@@ -1,4 +1,2 @@
 local plyMeta = FindMetaTable("Player")
-
 plyMeta.isMedic = fn.Compose{fn.Curry(fn.GetValue, 2)("medic"), plyMeta.getJobTable}
-local noMedicExists = fn.Compose{fn.Null, fn.Curry(fn.Filter, 2)(plyMeta.isMedic), player.GetAll}
