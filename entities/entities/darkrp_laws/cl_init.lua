812004efbe7488ae7ccd6e1fb570a44278cb23a8
Tenrys <tenrys.iaido@live.fr>
2014-02-01 17:50:00 +0100

    Tested, working.
    Laws are not in numerical order in the table, only in laws board.

--- entities/entities/darkrp_laws/cl_init.lua
+++ entities/entities/darkrp_laws/cl_init.lua
@@ -22,8 +22,8 @@ function ENT:Draw()
 
 		local col = Color(255, 255, 255, 255)
 		local lastHeight = 0
-		for _,v in ipairs(Laws) do
-			draw.DrawNonParsedText(v, "TargetID", 5, 35 + lastHeight, col)
+		for k,v in ipairs(Laws) do
+			draw.DrawNonParsedText(string.format("%s%s %s",k,".",v), "TargetID", 5, 35 + lastHeight, col)
 			lastHeight = lastHeight + ((fn.ReverseArgs(string.gsub(v, "\n", "")))+1)*21
 		end
 
@@ -33,7 +33,7 @@ end
 local function AddLaw(inLaw)
 	local law = DarkRP.textWrap(inLaw, "TargetID", 522)
 
-	Laws[#Laws + 1] = (#Laws + 1).. ". " .. law
+	Laws[#Laws + 1] = law
 end
 
 local function AddLawUM(um)
@@ -46,10 +46,10 @@ usermessage.Hook("DRP_AddLaw", AddLawUM)
 local function RemoveLaw(um)
 	local i = um:ReadShort()
 
-	hook.Run("removeLaw", i, string.sub(Laws[i], 4))
+	hook.Run("removeLaw", i, Laws[i])
 
 	while i < #Laws do
-		Laws[i] = i .. string.sub(Laws[i+1], (fn.ReverseArgs(string.find(Laws[i+1], "%d%."))))
+		Laws[i] = Laws[i+1]
 		i = i + 1
 	end
 	Laws[i] = nil
