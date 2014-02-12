9b3ff0e0b6549aa16a1d7c24f415e053c1ee62ae
TypicalRookie <TypicalRookie@users.noreply.github.com>
2014-02-12 15:02:12 -0800

    update stunstick SWEP.Instructions

--- entities/weapons/stunstick/shared.lua
+++ entities/weapons/stunstick/shared.lua
@@ -13,7 +13,7 @@ end
 SWEP.Base = "weapon_cs_base2"
 
 SWEP.Author = "DarkRP Developers"
-SWEP.Instructions = "Left click to discipline, right click to kill"
+SWEP.Instructions = "Left click to discipline\nRight click to kill"
 SWEP.Contact = ""
 SWEP.Purpose = ""
 SWEP.IconLetter = ""
