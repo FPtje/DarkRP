6f5d10118da22b71672354337de32c88943821b7
Bo Anderson <bo@toxicflames.co.uk>
2014-02-25 17:05:35 +0000

    Made FAdmin use DarkRP.formatMoney and removed dead code.

--- gamemode/modules/base/sh_util.lua
+++ gamemode/modules/base/sh_util.lua
@@ -35,15 +35,15 @@ function DarkRP.formatMoney(n)
 
 	if n >= 1e14 then return attachCurrency(tostring(n)) end
 
-    n = tostring(n)
-    local sep = sep or ","
-    local dp = string.find(n, "%.") or #n+1
+	n = tostring(n)
+	local sep = sep or ","
+	local dp = string.find(n, "%.") or #n+1
 
 	for i=dp-4, 1, -3 do
 		n = n:sub(1, i) .. sep .. n:sub(i+1)
-    end
+	end
 
-    return attachCurrency(n)
+	return attachCurrency(n)
 end
 
 /*---------------------------------------------------------------------------
