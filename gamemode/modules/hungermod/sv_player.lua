9c6c5745bf134e85045ad9f253b4951095977087
FPtje <fpeijnenburg@gmail.com>
2014-10-10 12:29:28 +0200

    Fixed error introduced in #1665

--- gamemode/modules/hungermod/sv_player.lua
+++ gamemode/modules/hungermod/sv_player.lua
@@ -9,7 +9,9 @@ end
 function meta:hungerUpdate()
 	if not IsValid(self) then return end
 	if not GAMEMODE.Config.hungerspeed then return end
-	self:setSelfDarkRPVar("Energy", math.Clamp(self:getDarkRPVar("Energy") - GAMEMODE.Config.hungerspeed, 0, 100))
+
+	local energy = self:getDarkRPVar("Energy")
+	self:setSelfDarkRPVar("Energy", energy and math.Clamp(energy - GAMEMODE.Config.hungerspeed, 0, 100) or 100)
 	self:GetTable().LastHungerUpdate = CurTime()
 
 	if self:getDarkRPVar("Energy") == 0 then
