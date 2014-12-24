3d168ab48d9bd08ef7385e6389394b2e3dca50b0
EMP <gunnarDebate@gmail.com>
2014-12-24 15:24:21 -0700

    Updated FAdmin admin chat
    Added AdminChat privilege and made the FAdmin Admin Chat use it.
    Changed the folder name to adminchat because thats what it actually has
    in it.

---
+++ gamemode/modules/fadmin/fadmin/adminchat/sv_init.lua
@@ -0,0 +1,26 @@
+util.AddNetworkString("FAdmin_ReceiveAdminMessage")
+local function ToAdmins(ply, cmd, args)
+	if not args[1] then return end
+
+	local text = table.concat(args, " ")
+	local send = {}
+
+	if ply ~= game.GetWorld() then table.insert(send, ply) end
+	for k,v in pairs(player.GetAll()) do
+		if FAdmin.Access.PlayerHasPrivilege(v, "AdminChat") then
+			table.insert(send, v)
+		end
+	end
+
+	net.Start("FAdmin_ReceiveAdminMessage")
+		net.WriteEntity(ply)
+		net.WriteString(text)
+	net.Send(send)
+end
+
+FAdmin.StartHooks["Chatting"] = function()
+	FAdmin.Commands.AddCommand("adminhelp", ToAdmins)
+	FAdmin.Commands.AddCommand("//", ToAdmins)
+	
+	FAdmin.Access.AddPrivilege("AdminChat", 2)
+end
