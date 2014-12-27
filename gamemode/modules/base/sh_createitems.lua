0df11e32183aad4ded0918605832af68c908ce76
FPtje <fpeijnenburg@gmail.com>
2014-12-27 17:31:39 +0100

    Fixed weird messages when messages not given

--- gamemode/modules/base/sh_createitems.lua
+++ gamemode/modules/base/sh_createitems.lua
@@ -433,8 +433,8 @@ function DarkRP.createJob(Name, colorOrTable, model, Description, Weapons, comma
 		}
 	CustomTeam.name = Name
 	-- Force CustomCheckFailMsg and canStartVoteReason into a function
-	CustomTeam.CustomCheckFailMsg = not isfunction(CustomTeam.CustomCheckFailMsg) and fp{fn.Id, CustomTeam.CustomCheckFailMsg} or CustomTeam.CustomCheckFailMsg
-	CustomTeam.canStartVoteReason = not isfunction(CustomTeam.canStartVoteReason) and fp{fn.Id, CustomTeam.canStartVoteReason} or CustomTeam.canStartVoteReason
+	CustomTeam.CustomCheckFailMsg = not isfunction(CustomTeam.CustomCheckFailMsg) and fp{fn.Id, CustomTeam.CustomCheckFailMsg, ""} or CustomTeam.CustomCheckFailMsg
+	CustomTeam.canStartVoteReason = not isfunction(CustomTeam.canStartVoteReason) and fp{fn.Id, CustomTeam.canStartVoteReason, ""} or CustomTeam.canStartVoteReason
 
 	-- Disabled job
 	if DarkRP.DARKRP_LOADING and DarkRP.disabledDefaults["jobs"][CustomTeam.command] then return end
@@ -505,7 +505,7 @@ function DarkRP.createShipment(name, model, entity, price, Amount_of_guns_in_one
 	end
 	customShipment.name = name
 	customShipment.allowed = customShipment.allowed or {}
-	customShipment.CustomCheckFailMsg = not isfunction(customShipment.CustomCheckFailMsg) and fp{fn.Id, customShipment.CustomCheckFailMsg} or customShipment.CustomCheckFailMsg
+	customShipment.CustomCheckFailMsg = not isfunction(customShipment.CustomCheckFailMsg) and fp{fn.Id, customShipment.CustomCheckFailMsg, ""} or customShipment.CustomCheckFailMsg
 
 	if DarkRP.DARKRP_LOADING and DarkRP.disabledDefaults["shipments"][customShipment.name] then return end
 
@@ -526,7 +526,7 @@ AddCustomShipment = DarkRP.createShipment
 function DarkRP.createVehicle(Name_of_vehicle, model, price, Jobs_that_can_buy_it, customcheck)
 	local vehicle = istable(Name_of_vehicle) and Name_of_vehicle or
 		{name = Name_of_vehicle, model = model, price = price, allowed = Jobs_that_can_buy_it, customCheck = customcheck}
-	vehicle.CustomCheckFailMsg = not isfunction(vehicle.CustomCheckFailMsg) and fp{fn.Id, vehicle.CustomCheckFailMsg} or vehicle.CustomCheckFailMsg
+	vehicle.CustomCheckFailMsg = not isfunction(vehicle.CustomCheckFailMsg) and fp{fn.Id, vehicle.CustomCheckFailMsg, ""} or vehicle.CustomCheckFailMsg
 
 	if DarkRP.DARKRP_LOADING and DarkRP.disabledDefaults["vehicles"][vehicle.name] then return end
 
@@ -564,7 +564,7 @@ function DarkRP.createEntity(name, entity, model, price, max, command, classes, 
 		{ent = entity, model = model, price = price, max = max,
 		cmd = command, allowed = classes, customCheck = CustomCheck}
 	tblEnt.name = name
-	tblEnt.CustomCheckFailMsg = not isfunction(tblEnt.CustomCheckFailMsg) and fp{fn.Id, tblEnt.CustomCheckFailMsg} or tblEnt.CustomCheckFailMsg
+	tblEnt.CustomCheckFailMsg = not isfunction(tblEnt.CustomCheckFailMsg) and fp{fn.Id, tblEnt.CustomCheckFailMsg, ""} or tblEnt.CustomCheckFailMsg
 
 	if DarkRP.DARKRP_LOADING and DarkRP.disabledDefaults["entities"][tblEnt.name] then return end
 
@@ -655,7 +655,7 @@ function DarkRP.createAmmoType(ammoType, name, model, price, amountGiven, custom
 		customCheck = customCheck
 	}
 	ammo.ammoType = ammoType
-	ammo.CustomCheckFailMsg = not isfunction(ammo.CustomCheckFailMsg) and fp{fn.Id, ammo.CustomCheckFailMsg} or ammo.CustomCheckFailMsg
+	ammo.CustomCheckFailMsg = not isfunction(ammo.CustomCheckFailMsg) and fp{fn.Id, ammo.CustomCheckFailMsg, ""} or ammo.CustomCheckFailMsg
 
 	if DarkRP.DARKRP_LOADING and DarkRP.disabledDefaults["ammo"][ammo.name] then return end
 	table.insert(gm.AmmoTypes, ammo)
