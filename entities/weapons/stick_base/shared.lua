766d22536a866c60187e0a7b8b4d89cce7d558e6
Bo Anderson <bo@toxicflames.co.uk>
2015-04-03 15:25:48 +0100

    Refactored arrest, unarrest and stunsticks to a common stick_base.

---
+++ entities/weapons/stick_base/shared.lua
@@ -0,0 +1,159 @@
+AddCSLuaFile()
+
+if CLIENT then
+	SWEP.DrawAmmo = false
+	SWEP.DrawCrosshair = false
+end
+
+DEFINE_BASECLASS("weapon_cs_base2")
+
+SWEP.Author = "DarkRP Developers"
+SWEP.Contact = ""
+SWEP.Purpose = ""
+SWEP.IconLetter = ""
+
+SWEP.ViewModelFOV = 62
+SWEP.ViewModelFlip = false
+SWEP.AnimPrefix = "stunstick"
+
+SWEP.AdminOnly = true
+
+SWEP.StickColor = Color(255, 255, 255)
+
+SWEP.ViewModel = Model("models/weapons/v_stunbaton.mdl")
+SWEP.WorldModel = Model("models/weapons/w_stunbaton.mdl")
+
+SWEP.Sound = Sound("weapons/stunstick/stunstick_swing1.wav")
+
+SWEP.Primary.ClipSize = -1
+SWEP.Primary.DefaultClip = 0
+SWEP.Primary.Automatic = false
+SWEP.Primary.Ammo = ""
+
+SWEP.Secondary.ClipSize = -1
+SWEP.Secondary.DefaultClip = 0
+SWEP.Secondary.Automatic = false
+SWEP.Secondary.Ammo = ""
+
+function SWEP:Initialize()
+	self:SetHoldType("normal")
+end
+
+function SWEP:SetupDataTables()
+	BaseClass.SetupDataTables(self)
+	-- Bool 0 = Ironsights
+	-- Bool 1 = Reloading
+	self:NetworkVar("Bool", 2, "SeqIdling")
+	-- Float 0 = LastPrimaryAttack
+	-- Float 1 = ReloadEndTime
+	-- Float 2 = BurstTime
+	-- Float 3 = LastNonBurst
+	self:NetworkVar("Float", 4, "SeqIdleTime")
+	self:NetworkVar("Float", 5, "HoldTypeChangeTime")
+end
+
+function SWEP:StartCommand()
+end
+
+function SWEP:HookStartCommand()
+	hook.Add("StartCommand", "DarkRP_"..self:GetClass().."_StartCommand", fp{self.StartCommand, self})
+end
+
+function SWEP:Deploy()
+	BaseClass.Deploy(self)
+	if SERVER then
+		self:HookStartCommand()
+		self:CallOnClient("HookStartCommand")
+		self:SetColor(self.StickColor)
+		self:SetMaterial("models/shiny")
+	end
+	local vm = self:GetOwner():GetViewModel()
+	if not IsValid(vm) then return true end
+	self:PreDrawViewModel()
+	vm:SendViewModelMatchingSequence(vm:LookupSequence("idle01"))
+	return true
+end
+
+function SWEP:PreDrawViewModel()
+	if SERVER or not IsValid(self:GetOwner()) or not IsValid(self:GetOwner():GetViewModel()) then return end
+	self:GetOwner():GetViewModel():SetColor(self.StickColor)
+	self:GetOwner():GetViewModel():SetMaterial("models/shiny")
+end
+
+function SWEP:ResetStick(force)
+	if not IsValid(self:GetOwner()) or (not force and (not IsValid(self:GetOwner():GetActiveWeapon()) or self:GetOwner():GetActiveWeapon():GetClass() ~= self:GetClass())) then return end
+	if SERVER then
+		self:SetColor(Color(255, 255, 255))
+		self:SetMaterial("")
+	elseif IsValid(self:GetOwner()) and IsValid(self:GetOwner():GetViewModel()) then
+		self:GetOwner():GetViewModel():SetColor(Color(255, 255, 255))
+		self:GetOwner():GetViewModel():SetMaterial("")
+	end
+	self:SetSeqIdling(false)
+	self:SetSeqIdleTime(0)
+	self:SetHoldTypeChangeTime(0)
+	hook.Remove("StartCommand", "DarkRP_"..self:GetClass().."_StartCommand")
+end
+
+function SWEP:Holster()
+	BaseClass.Holster(self)
+	self:ResetStick(false)
+	return true
+end
+
+function SWEP:OnRemove()
+	BaseClass.OnRemove(self)
+	self:ResetStick(true)
+end
+
+function SWEP:Think()
+	if self:GetSeqIdling() then
+		self:SetSeqIdling(false)
+
+		if not IsValid(self:GetOwner()) then return end
+		self:GetOwner():SetAnimation(PLAYER_ATTACK1)
+		self:EmitSound(self.Sound)
+
+		local vm = self:GetOwner():GetViewModel()
+		if not IsValid(vm) then return end
+		vm:SendViewModelMatchingSequence(vm:LookupSequence("attackch"))
+		vm:SetPlaybackRate(1 + 1/3)
+		local duration = vm:SequenceDuration() / vm:GetPlaybackRate()
+		local time = CurTime() + duration
+		self:SetSeqIdleTime(time)
+		self:SetNextPrimaryFire(time)
+	end
+	if self:GetSeqIdleTime() ~= 0 and CurTime() >= self:GetSeqIdleTime() then
+		self:SetSeqIdleTime(0)
+
+		if not IsValid(self:GetOwner()) then return end
+		local vm = self:GetOwner():GetViewModel()
+		if not IsValid(vm) then return end
+		vm:SendViewModelMatchingSequence(vm:LookupSequence("idle01"))
+	end
+	if self:GetHoldTypeChangeTime() ~= 0 and CurTime() >= self:GetHoldTypeChangeTime() then
+		self:SetHoldTypeChangeTime(0)
+		self:SetHoldType("normal")
+	end
+end
+
+function SWEP:PrimaryAttack()
+	self:SetHoldType("melee")
+	self:SetHoldTypeChangeTime(CurTime() + 0.3)
+
+	self:SetNextPrimaryFire(CurTime() + 0.51) -- Actual delay is set later.
+
+	local vm = self:GetOwner():GetViewModel()
+	if IsValid(vm) then
+		vm:SendViewModelMatchingSequence(vm:LookupSequence("idle01"))
+		self:SetSeqIdling(true)
+	end
+end
+
+function SWEP:SecondaryAttack()
+	-- Do nothing
+end
+
+function SWEP:Reload()
+	-- Do nothing
+end
--- entities/weapons/stunstick/shared.lua
+++ entities/weapons/stunstick/shared.lua
@@ -4,45 +4,19 @@ if CLIENT then
 	SWEP.PrintName = "Stun Stick"
 	SWEP.Slot = 0
 	SWEP.SlotPos = 5
-	SWEP.DrawAmmo = false
-	SWEP.DrawCrosshair = false
 end
 
-SWEP.Base = "weapon_cs_base2"
+DEFINE_BASECLASS("stick_base")
 
-SWEP.Author = "DarkRP Developers"
 SWEP.Instructions = "Left click to discipline\nRight click to kill\nReload to threaten"
-SWEP.Contact = ""
-SWEP.Purpose = ""
-SWEP.IconLetter = ""
-
-SWEP.ViewModelFOV = 62
-SWEP.ViewModelFlip = false
-SWEP.AnimPrefix = "stunstick"
 
 SWEP.Spawnable = true
-SWEP.AdminOnly = true
 SWEP.Category = "DarkRP (Utility)"
 
-SWEP.NextStrike = 0
-
-SWEP.ViewModel = Model("models/weapons/v_stunbaton.mdl")
-SWEP.WorldModel = Model("models/weapons/w_stunbaton.mdl")
-
-SWEP.Sound = Sound("weapons/stunstick/stunstick_swing1.wav")
-
-SWEP.Primary.ClipSize = -1
-SWEP.Primary.DefaultClip = 0
-SWEP.Primary.Automatic = false
-SWEP.Primary.Ammo = ""
-
-SWEP.Secondary.ClipSize = -1
-SWEP.Secondary.DefaultClip = 0
-SWEP.Secondary.Automatic = false
-SWEP.Secondary.Ammo = ""
+SWEP.StickColor = Color(0, 0, 255)
 
 function SWEP:Initialize()
-	self:SetHoldType("normal")
+	BaseClass.Initialize(self)
 
 	self.Hit = {
 		Sound("weapons/stunstick/stunstick_impact1.wav"),
@@ -55,44 +29,15 @@ function SWEP:Initialize()
 	}
 end
 
-function SWEP:Deploy()
-	if SERVER then
-		self:SetColor(Color(0,0,255,255))
-		self:SetMaterial("models/shiny")
-		local vm = self.Owner:GetViewModel()
-		if not IsValid(vm) then return end
-		vm:ResetSequence(vm:LookupSequence("idle01"))
-	end
-	return true
-end
-
-function SWEP:PreDrawViewModel()
-	if SERVER or not IsValid(self.Owner) or not IsValid(self.Owner:GetViewModel()) then return end
-	self.Owner:GetViewModel():SetColor(Color(0,0,255,255))
-	self.Owner:GetViewModel():SetMaterial("models/shiny")
-end
-
-function SWEP:Holster()
-	if SERVER then
-		self:SetColor(Color(255,255,255,255))
-		self:SetMaterial("")
-		timer.Stop(self:GetClass() .. "_idle" .. self:EntIndex())
-	elseif CLIENT and IsValid(self.Owner) and IsValid(self.Owner:GetViewModel()) then
-		self.Owner:GetViewModel():SetColor(Color(255,255,255,255))
-		self.Owner:GetViewModel():SetMaterial("")
-	end
-	return true
-end
-
-function SWEP:OnRemove()
-	if SERVER then
-		self:SetColor(Color(255,255,255,255))
-		self:SetMaterial("")
-		timer.Stop(self:GetClass() .. "_idle" .. self:EntIndex())
-	elseif CLIENT and IsValid(self.Owner) and IsValid(self.Owner:GetViewModel()) then
-		self.Owner:GetViewModel():SetColor(Color(255,255,255,255))
-		self.Owner:GetViewModel():SetMaterial("")
-	end
+function SWEP:SetupDataTables()
+	BaseClass.SetupDataTables(self)
+	-- Float 0 = LastPrimaryAttack
+	-- Float 1 = ReloadEndTime
+	-- Float 2 = BurstTime
+	-- Float 3 = LastNonBurst
+	-- Float 4 = SeqIdleTime
+	-- Float 5 = HoldTypeChangeTime
+	self:NetworkVar("Float", 6, "LastReload")
 end
 
 function SWEP:DoFlash(ply)
@@ -103,101 +48,65 @@ end
 
 local entMeta = FindMetaTable("Entity")
 function SWEP:DoAttack(dmg)
-	if CurTime() < self.NextStrike then return end
-
-	self:SetHoldType("melee")
-	timer.Simple(0.3, function() if self:IsValid() then self:SetHoldType("normal") end end)
-
-	self.NextStrike = CurTime() + 0.51 -- Actual delay is set later.
-
 	if CLIENT then return end
 
-	timer.Stop(self:GetClass() .. "_idle" .. self:EntIndex())
-	local vm = self.Owner:GetViewModel()
-	if IsValid(vm) then
-		vm:ResetSequence(vm:LookupSequence("idle01"))
-		timer.Simple(0, function()
-			if not IsValid(self) or not IsValid(self.Owner) or not IsValid(self.Owner:GetActiveWeapon()) or self.Owner:GetActiveWeapon() ~= self then return end
-			self.Owner:SetAnimation(PLAYER_ATTACK1)
-
-			if IsValid(self.Weapon) then
-				self.Weapon:EmitSound(self.Sound)
-			end
-
-			local vm = self.Owner:GetViewModel()
-			if not IsValid(vm) then return end
-			vm:ResetSequence(vm:LookupSequence("attackch"))
-			vm:SetPlaybackRate(1 + 1/3)
-			local duration = vm:SequenceDuration() / vm:GetPlaybackRate()
-			timer.Create(self:GetClass() .. "_idle" .. self:EntIndex(), duration, 1, function()
-				if not IsValid(self) or not IsValid(self.Owner) then return end
-				local vm = self.Owner:GetViewModel()
-				if not IsValid(vm) then return end
-				vm:ResetSequence(vm:LookupSequence("idle01"))
-			end)
-			self.NextStrike = CurTime() + duration
-		end)
-	end
-
-	self.Owner:LagCompensation(true)
-	local trace = util.QuickTrace(self.Owner:EyePos(), self.Owner:GetAimVector() * 90, {self.Owner})
-	self.Owner:LagCompensation(false)
+	self:GetOwner():LagCompensation(true)
+	local trace = util.QuickTrace(self:GetOwner():EyePos(), self:GetOwner():GetAimVector() * 90, {self:GetOwner()})
+	self:GetOwner():LagCompensation(false)
 	if IsValid(trace.Entity) and trace.Entity.onStunStickUsed then
-		trace.Entity:onStunStickUsed(self.Owner)
+		trace.Entity:onStunStickUsed(self:GetOwner())
 		return
 	elseif IsValid(trace.Entity) and trace.Entity:GetClass() == "func_breakable_surf" then
 		trace.Entity:Fire("Shatter")
-		self.Owner:EmitSound(self.Hit[math.random(1,#self.Hit)])
+		self:GetOwner():EmitSound(self.Hit[math.random(1,#self.Hit)])
 		return
 	end
 
-	local ent = self.Owner:getEyeSightHitEntity(100, 15, fn.FAnd{fp{fn.Neq, self.Owner}, fc{IsValid, entMeta.GetPhysicsObject}})
+	local ent = self:GetOwner():getEyeSightHitEntity(100, 15, fn.FAnd{fp{fn.Neq, self:GetOwner()}, fc{IsValid, entMeta.GetPhysicsObject}})
 
 	if not IsValid(ent) then return end
 
 	if not ent:isDoor() then
-		ent:SetVelocity((ent:GetPos() - self.Owner:GetPos()) * 7)
+		ent:SetVelocity((ent:GetPos() - self:GetOwner():GetPos()) * 7)
 	end
 
 	if dmg > 0 then
-		ent:TakeDamage(dmg, self.Owner, self)
+		ent:TakeDamage(dmg, self:GetOwner(), self)
 	end
 
 	if ent:IsPlayer() or ent:IsNPC() or ent:IsVehicle() then
-		self.DoFlash(self, ent)
-		self.Owner:EmitSound(self.FleshHit[math.random(1,#self.FleshHit)])
+		self:DoFlash(ent)
+		self:GetOwner():EmitSound(self.FleshHit[math.random(1,#self.FleshHit)])
 	else
-		self.Owner:EmitSound(self.Hit[math.random(1,#self.Hit)])
-		if FPP and FPP.plyCanTouchEnt(self.Owner, ent, "EntityDamage") then
-			if ent.SeizeReward and not ent.beenSeized and not ent.burningup and self.Owner:isCP() and ent.Getowning_ent and self.Owner ~= ent:Getowning_ent() then
-				self.Owner:addMoney(ent.SeizeReward)
-				DarkRP.notify(self.Owner, 1, 4, DarkRP.getPhrase("you_received_x", DarkRP.formatMoney(ent.SeizeReward), DarkRP.getPhrase("bonus_destroying_entity")))
+		self:GetOwner():EmitSound(self.Hit[math.random(1,#self.Hit)])
+		if FPP and FPP.plyCanTouchEnt(self:GetOwner(), ent, "EntityDamage") then
+			if ent.SeizeReward and not ent.beenSeized and not ent.burningup and self:GetOwner():isCP() and ent.Getowning_ent and self:GetOwner() ~= ent:Getowning_ent() then
+				self:GetOwner():addMoney(ent.SeizeReward)
+				DarkRP.notify(self:GetOwner(), 1, 4, DarkRP.getPhrase("you_received_x", DarkRP.formatMoney(ent.SeizeReward), DarkRP.getPhrase("bonus_destroying_entity")))
 				ent.beenSeized = true
 			end
-			ent:TakeDamage(1000-dmg, self.Owner, self) -- for illegal entities
+			ent:TakeDamage(1000-dmg, self:GetOwner(), self) -- for illegal entities
 		end
 	end
 end
 
 function SWEP:PrimaryAttack()
+	BaseClass.PrimaryAttack(self)
+	self:SetNextSecondaryFire(self:GetNextPrimaryFire())
 	self:DoAttack(0)
 end
 
 function SWEP:SecondaryAttack()
+	BaseClass.PrimaryAttack(self)
+	self:SetNextSecondaryFire(self:GetNextPrimaryFire())
 	self:DoAttack(10)
 end
 
 function SWEP:Reload()
 	self:SetHoldType("melee")
-	timer.Destroy("rp_stunstick_threaten")
-	timer.Create("rp_stunstick_threaten", 1, 1, function()
-		if not IsValid(self) then return end
-		self:SetHoldType("normal")
-	end)
+	self:SetHoldTypeChangeTime(CurTime() + 1)
 
-	if not SERVER then return end
-
-	if self.LastReload and self.LastReload > CurTime() - 0.1 then self.LastReload = CurTime() return end
-	self.LastReload = CurTime()
-	self.Owner:EmitSound("weapons/stunstick/spark"..math.random(1,3)..".wav")
+	if self:GetLastReload() + 0.1 > CurTime() then self:SetLastReload(CurTime()) return end
+	self:SetLastReload(CurTime())
+	self:EmitSound("weapons/stunstick/spark"..math.random(1,3)..".wav")
 end
