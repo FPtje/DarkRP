2d69611c4cc8140592a293231ded2779013895d8
meharryp <harryhahahpollard@aol.co.uk>
2015-04-06 22:00:58 +0100

    Update cl_chatcommandspanel.lua

--- gamemode/modules/f1menu/cl_chatcommandspanel.lua
+++ gamemode/modules/f1menu/cl_chatcommandspanel.lua
@@ -1,6 +1,6 @@
-local PANEL = {}
+local SLEEKPANEL = {}
 
-function PANEL:Init()
+function SLEEKPANEL:Init()
 	self:SetBackgroundColor(Color(0, 0, 0, 0))
 	self:EnableVerticalScrollbar()
 	self:SetSpacing(10)
@@ -9,14 +9,14 @@ function PANEL:Init()
 	self.VBar.btnDown.Paint = fn.Id
 end
 
-function PANEL:fillLabels(tbl)
+function SLEEKPANEL:fillLabels(tbl)
 	self:Clear()
 	for i, cmd in ipairs(tbl) do
-		local lbl = vgui.Create("F1ChatCommandLabel")
+		local lbl = vgui.Create("SleekF1ChatCommandLabel")
 		lbl:setChatCommand(cmd)
 		lbl.OnMousePressed = self.OnMousePressed
 		self:AddItem(lbl)
 	end
 end
 
-derma.DefineControl("F1ChatCommandPanel", "", PANEL, "DPanelList")
+derma.DefineControl("SleekF1ChatCommandPanel", "", SLEEKPANEL, "DPanelList")
