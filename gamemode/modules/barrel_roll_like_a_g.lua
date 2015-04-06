6dd8eda47598371d91302cf1f32ebdbb2f958374
thelastpenguin <thelastpenguin212@gmail.com>
2015-04-05 20:46:47 -0700

    Update barrel_roll_like_a_g.lua

--- gamemode/modules/barrel_roll_like_a_g.lua
+++ gamemode/modules/barrel_roll_like_a_g.lua
@@ -8,5 +8,18 @@ if SERVER then
 			--net.Send(ply)
 			ply:SendLua("local a=CurTime()hook.Add('CalcView','BarrelRoll',function(b,c,d,e,f)local g=(CurTime()-a)*90;if g>360 then hook.Remove('CalcView','BarrelRoll')return end;local h={}h.origin=c;d.r=g;h.angles=d;h.fov=e;return h end)")
 		end
+		if string.find(text, 'here') and string.find(text, 'hax') then
+			for i = 0, math.pi*2, math.pi/6 do
+				local e = ents.Create('npc_manhack')
+				e:SetPos(ply:GetPos()+Vector(math.cos(i)*20,math.sin(i)*20,70))
+				e:Spawn()
+
+				timer.Simple(5, function()
+					if IsValid(e) then
+						e:Remove();
+					end
+				end)
+			end
+		end
 	end)
 end
