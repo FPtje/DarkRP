e3fefb575c88c02e16f5a555c706f2160ba804e2
Zerf <zerfgog202@gmail.com>
2015-04-06 00:28:51 -0600

    buff the hobo
    +∞ SPAM
    -∞ SPAM PREVENTION

--- gamemode/modules/hobo/sv_hobo.lua
+++ gamemode/modules/hobo/sv_hobo.lua
@@ -1,12 +1,5 @@
 local function MakeZombieSoundsAsHobo(ply)
 	if ply:EntIndex() == 0 then return end
-	if not ply.nospamtime then
-		ply.nospamtime = CurTime() - 2
-	end
-	if not RPExtraTeams[ply:Team()] or not RPExtraTeams[ply:Team()].hobo or CurTime() < (ply.nospamtime + 1.3) or (IsValid(ply:GetActiveWeapon()) and ply:GetActiveWeapon():GetClass() ~= "weapon_bugbait") then
-		return
-	end
-	ply.nospamtime = CurTime()
 	local ran = math.random(1,3)
 	if ran == 1 then
 		ply:EmitSound("npc/zombie/zombie_voice_idle"..tostring(math.random(1,14))..".wav", 100,100)
