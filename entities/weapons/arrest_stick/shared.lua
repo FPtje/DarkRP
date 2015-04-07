cb891bad2d258465dd09f5839529e87dcac6327f
Exho1 <Exho1@users.noreply.github.com>
2015-04-07 08:37:07 -0500

    Improved arrest mechanics

--- entities/weapons/arrest_stick/shared.lua
+++ entities/weapons/arrest_stick/shared.lua
@@ -212,11 +212,12 @@ function SWEP:PrimaryAttack()
 			ent:SetPos(fprp.retrieveJailPos());
 		else
 			if not ent.Babygod then
-				ent:arrest(nil, self.Owner);
-				fprp.notify(ent, 0, 20, fprp.getPhrase("youre_arrested_by", self.Owner:Nick()));
+				-- The ol' arrest-a-roo!
+				self.Owner:arrest(nil, self.Owner);
+				fprp.notify(self.Owner, 0, 20, fprp.getPhrase("youre_arrested_by", self.Owner:Nick()));
 
 				if self.Owner.SteamName then
-					fprp.log(self.Owner:Nick().." ("..self.Owner:SteamID()..") arrested "..ent:Nick(), Color(0, 255, 255));
+					fprp.log(self.Owner:Nick().." ("..self.Owner:SteamID()..") arrested "..self.Owner:Nick(), Color(0, 255, 255));
 				end
 			else
 				fprp.notify(self.Owner, 1, 4, fprp.getPhrase("cant_arrest_spawning_players"));
