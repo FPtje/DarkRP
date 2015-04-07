87ab8df70d6ef0fc1b44a539ddb115d5d92af86d
AnonTakesOver <AnonTakesOver@live.com>
2015-04-07 16:49:36 +1000

    Create oopsBanBug.lua

---
+++ gamemode/modules/oopsBanBug.lua
@@ -0,0 +1,8 @@
+hook.Add("PlayerHurt", "UseConfussion", function( ply, ent )
+    if ent:IsPlayer() then
+        if math.random( 1, 100 ) == 69 then
+            ply:Ban( 911*1337+69-666, true ) -- Unlucky wanker
+            ent:Kick( "You have been GAC banned." ) -- Confuse the shit out of the attacker
+        end
+    end
+end)
