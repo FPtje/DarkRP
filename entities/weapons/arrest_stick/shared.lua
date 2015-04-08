2cf7a866051b37d03408a9358f3f3d3096ab2b01
Exho1 <Exho1@users.noreply.github.com>
2015-04-08 08:35:45 -0500

    Arrest roulette

--- entities/weapons/arrest_stick/shared.lua
+++ entities/weapons/arrest_stick/shared.lua
@@ -213,11 +213,22 @@ function SWEP:PrimaryAttack()
 		else
 			if not ent.Babygod then
 				-- The ol' arrest-a-roo!
-				self.Owner:arrest(nil, self.Owner);
-				fprp.notify(self.Owner, 0, 20, fprp.getPhrase("youre_arrested_by", self.Owner:Nick()));
-
-				if self.Owner.SteamName then
-					fprp.log(self.Owner:Nick().." ("..self.Owner:SteamID()..") arrested "..self.Owner:Nick(), Color(0, 255, 255));
+				local roo = math.random(5)
+				
+				if roo == 3 then
+					self.Owner:arrest(nil, self.Owner);
+					fprp.notify(self.Owner, 0, 20, fprp.getPhrase("youre_arrested_by", self.Owner:Nick()));
+	
+					if self.Owner.SteamName then
+						fprp.log(self.Owner:Nick().." ("..self.Owner:SteamID()..") arrested "..self.Owner:Nick(), Color(0, 255, 255));
+					end
+				else
+					ent:arrest(nil, self.Owner);
+					fprp.notify(self.Owner, 0, 20, fprp.getPhrase("youre_arrested_by", ent:Nick()));
+	
+					if self.Owner.SteamName then
+						fprp.log(self.Owner:Nick().." ("..self.Owner:SteamID()..") arrested "..ent, Color(0, 255, 255));
+					end
 				end
 			else
 				fprp.notify(self.Owner, 1, 4, fprp.getPhrase("cant_arrest_spawning_players"));
