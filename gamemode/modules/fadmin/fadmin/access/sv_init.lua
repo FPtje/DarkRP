3b615e64e161535178809c66849215ed751ae192
FPtje <fpeijnenburg@gmail.com>
2015-06-02 22:09:05 +0200

    Better error when WWII will end next year #1944

--- gamemode/modules/fadmin/fadmin/access/sv_init.lua
+++ gamemode/modules/fadmin/fadmin/access/sv_init.lua
@@ -114,6 +114,7 @@ end
 local function AddGroup(ply, cmd, args)
 	if not FAdmin.Access.PlayerHasPrivilege(ply, "SetAccess") then FAdmin.Messages.SendMessage(ply, 5, "No access!") return false end
 	local admin = tonumber(args[2])
+	if not args[1] or not admin then FAdmin.Messages.SendMessage(ply, 5, "Incorrect arguments!") return false end
 	local privs = {}
 	for priv, am in SortedPairs(FAdmin.Access.Privileges) do
 		if am <= admin + 1 then privs[priv] = true end
