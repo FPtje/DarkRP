be5dea5fda79d097e5f4e91edce94854a0679bef
RoboDerpy <roboderpy@gmail.com>
2015-08-31 15:05:15 +0700

    Spaces

--- entities/entities/money_printer/cl_init.lua
+++ entities/entities/money_printer/cl_init.lua
@@ -23,8 +23,8 @@ function ENT:Draw()
     Ang:RotateAroundAxis(Ang:Up(), 90)
 
     cam.Start3D2D(Pos + Ang:Up() * 11.5, Ang, 0.11)
-        draw.WordBox(2, -TextWidth*0.5, -30, text, "HUDNumber5", Color(140, 0, 0, 100), Color(255,255,255,255))
-        draw.WordBox(2, -TextWidth2*0.5, 18, owner, "HUDNumber5", Color(140, 0, 0, 100), Color(255,255,255,255))
+        draw.WordBox(2, -TextWidth * 0.5, -30, text, "HUDNumber5", Color(140, 0, 0, 100), Color(255, 255, 255, 255))
+        draw.WordBox(2, -TextWidth2 * 0.5, 18, owner, "HUDNumber5", Color(140, 0, 0, 100), Color(255, 255, 255, 255))
     cam.End3D2D()
 end
 
