1e6a8fad02319a5bb0c8c1b0a61aa1faa13a8145
Matthew Nameful <mateusz.kmiec.2015@gmail.com>
2015-09-02 23:31:49 +0200

    Made the description more consistent

--- gamemode/modules/base/sh_commands.lua
+++ gamemode/modules/base/sh_commands.lua
@@ -65,6 +65,6 @@ DarkRP.declareChatCommand{
 
 DarkRP.declareChatCommand{
     command = "freerpname",
-    description = "Remove an rp name from the database so a player can use it",
+    description = "Remove a RP name from the database so a player can use it",
     delay = 1.5
 }
