de2c0e75b0d314a45a226f18a4352c8aad37ad33
FPtje <fpeijnenburg@gmail.com>
2015-09-14 21:59:50 +0200

    Re-fixed spaces in command args

--- gamemode/modules/chat/sv_chat.lua
+++ gamemode/modules/chat/sv_chat.lua
@@ -180,6 +180,13 @@ hook.Add("InitPostEntity", "RemoveChatHooks", ReplaceChatHooks)
 local function ConCommand(ply, _, args)
     if not args[1] then return end
 
+    -- Put quotes around the args that have spaces in them
+    for i, a in pairs(args) do
+        if string.find(a, " ") then
+            args[i] = '"' .. a .. '"'
+        end
+    end
+
     local cmd = string.lower(args[1])
     local arg = table.concat(args, ' ', 2)
     local tbl = DarkRP.getChatCommand(cmd)
