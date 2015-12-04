1bb4376cc5274502bc23f1e617bd6259c4c1aaaa
FPtje <fpeijnenburg@gmail.com>
2015-12-04 22:06:26 +0100

    Merged functional library

--- gamemode/libraries/fn.lua
+++ gamemode/libraries/fn.lua
@@ -37,6 +37,7 @@ local ipairs = ipairs
 local error = error
 local math = math
 local select = select
+local type = type
 local _G = _G
 local fp = fp
 
@@ -75,14 +76,22 @@ end
 Misc functions
 ---------------------------------------------------------------------------*/
 -- function composition
-Compose = function(funcs)
-    return function(...)
-        local res = {...}
-        for i = #funcs, 1, -1 do
-            res = {funcs[i](unpack(res))}
-        end
-        return unpack(res)
-    end
+-- function composition
+do
+	local function comp_h(a, b, ...)
+		if b == nil then return a end
+		b = comp_h(b, ...)
+		return function(...)
+			return a(b(...))
+		end
+	end
+	Compose = function(funcs, ...)
+		if type(funcs) == 'table' then
+			return comp_h(unpack(funcs))
+		else
+			return comp_h(funcs, ...)
+		end
+	end
 end
 
 _G.fc = Compose
