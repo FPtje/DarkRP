858d744815657d44d5f40e4ea5cf4cf3768365bb
FPtje <fpeijnenburg@gmail.com>
2015-12-09 21:29:25 +0100

    Reduced scope depth in svchatcommands

--- gamemode/modules/fadmin/fadmin/commands/sv_chatcommands.lua
+++ gamemode/modules/fadmin/fadmin/commands/sv_chatcommands.lua
@@ -1,25 +1,25 @@
 local prefix = "/"
 hook.Add("PlayerSay", "FAdminChatCommands", function(ply, text, Team, dead)
-    if string.sub(text, 1, 1) == prefix then
-        local TExplode = string.Explode(" ", string.sub(text, 2))
-        if not TExplode then return end
+    if string.sub(text, 1, 1) ~= prefix then return end
 
-        for k, v in pairs(TExplode) do
-            if string.sub(v, -1) == "," and TExplode[k + 1] then
-                TExplode[k] = (TExplode[k] or "") .. (TExplode[k + 1] or "")
-                table.remove(TExplode, k + 1)
-            end
-        end
-        table.ClearKeys(TExplode, false)
+    local TExplode = string.Explode(" ", string.sub(text, 2))
+    if not TExplode then return end
 
-        local Command = string.lower(TExplode[1])
-        local Args = table.Copy(TExplode)
-        Args[1] = nil
-        Args = table.ClearKeys(Args)
-        if FAdmin.Commands.List[Command] then
-            local res = {FAdmin.Commands.List[Command].callback(ply, Command, Args)}
-            hook.Call("FAdmin_OnCommandExecuted", nil, ply, Command, Args, res)
-            return ""
+    for k, v in pairs(TExplode) do
+        if string.sub(v, -1) == "," and TExplode[k + 1] then
+            TExplode[k] = (TExplode[k] or "") .. (TExplode[k + 1] or "")
+            table.remove(TExplode, k + 1)
         end
+    end
+    table.ClearKeys(TExplode, false)
+
+    local Command = string.lower(TExplode[1])
+    local Args = table.Copy(TExplode)
+    Args[1] = nil
+    Args = table.ClearKeys(Args)
+    if FAdmin.Commands.List[Command] then
+        local res = {FAdmin.Commands.List[Command].callback(ply, Command, Args)}
+        hook.Call("FAdmin_OnCommandExecuted", nil, ply, Command, Args, res)
+        return ""
     end
 end)
