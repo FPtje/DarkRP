ab9d154405598c49a40f781f126609cd676d7c99
Mad Kernel <amd@default.im>
2016-03-10 18:49:25 +0200

    Ability to check successful lockdown init

--- gamemode/modules/police/sv_commands.lua
+++ gamemode/modules/police/sv_commands.lua
@@ -131,22 +131,22 @@ function DarkRP.lockdown(ply)
     local show = ply:EntIndex() == 0 and print or fp{DarkRP.notify, ply, 1, 4}
     if GetGlobalBool("DarkRP_LockDown") then
         show(DarkRP.getPhrase("unable", "/lockdown", DarkRP.getPhrase("stop_lockdown")))
-        return ""
+        return
     end
 
     if ply:EntIndex() ~= 0 and not ply:isMayor() then
         show(DarkRP.getPhrase("incorrect_job", "/lockdown", ""))
-        return ""
+        return
     end
 
     if not GAMEMODE.Config.lockdown then
         show(DarkRP.getPhrase("disabled", "lockdown", ""))
-        return ""
+        return
     end
 
     if lastLockdown > CurTime() - GAMEMODE.Config.lockdowndelay then
         show(DarkRP.getPhrase("wait_with_that"))
-        return ""
+        return
     end
 
     for _, v in pairs(player.GetAll()) do
@@ -157,7 +157,7 @@ function DarkRP.lockdown(ply)
     SetGlobalBool("DarkRP_LockDown", true)
     DarkRP.notifyAll(0, 3, DarkRP.getPhrase("lockdown_started"))
 
-    return ""
+    return true
 end
 DarkRP.defineChatCommand("lockdown", DarkRP.lockdown)
 
