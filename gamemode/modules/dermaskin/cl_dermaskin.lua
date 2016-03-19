57e7c146be60667f03442a820db86b66e5bfb183
Bo Anderson <bo@toxicflames.co.uk>
2016-03-19 09:56:27 +0000

    Fixed SKIN support for F1 search box.

--- gamemode/modules/dermaskin/cl_dermaskin.lua
+++ gamemode/modules/dermaskin/cl_dermaskin.lua
@@ -7,6 +7,11 @@ SKIN.DermaVersion       = 1
 SKIN.GwenTexture        = Material("darkrp/darkrpderma.png")
 
 
+SKIN.colTextEntryText               = Color(255, 255, 255)
+SKIN.colTextEntryTextCursor         = Color(255, 255, 255)
+
+SKIN.colTextEntryTextPlaceholder    = Color(200, 200, 200, 200) -- Unofficial but will probably be named this
+
 SKIN.tex = {}
 
 SKIN.tex.Selection                  = GWEN.CreateTextureBorder(384, 32, 31, 31, 4, 4, 4, 4)
@@ -237,5 +242,23 @@ SKIN.Colours.Category.LineAlt.Text_Selected     = GWEN.TextureColor(4 + 8 * 24, 
 SKIN.Colours.Category.LineAlt.Button            = GWEN.TextureColor(4 + 8 * 25, 508)
 SKIN.Colours.Category.LineAlt.Button_Hover      = GWEN.TextureColor(4 + 8 * 24, 500)
 SKIN.Colours.Category.LineAlt.Button_Selected   = GWEN.TextureColor(4 + 8 * 25, 500)
+
+-- Remove the following two functions when https://github.com/garrynewman/garrysmod/pull/1141 is merged
+function SKIN:PaintTextEntry(panel, w, h)
+    if panel.m_bBackground then
+        if panel:GetDisabled() then
+            self.tex.TextBox_Disabled(0, 0, w, h)
+        elseif panel:HasFocus() then
+            self.tex.TextBox_Focus(0, 0, w, h)
+        else
+            self.tex.TextBox(0, 0, w, h)
+        end
+    end
+
+    panel:DrawTextEntryText(panel.m_colText or self.colTextEntryText, panel.m_colHighlight or self.colTextEntryTextHighlight, panel.m_colCursor or self.colTextEntryTextCursor)
+end
+
+function SKIN:SchemeTextEntry()
+end
 
 derma.DefineSkin("DarkRP", "The official SKIN for DarkRP", SKIN)
