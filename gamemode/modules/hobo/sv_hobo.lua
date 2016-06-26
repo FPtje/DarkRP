fa310ccdfb317b166af606a4ea5ca159499c569c
kiruthekiller <danila.shamaev@gmail.com>
2016-06-26 22:04:21 +0300

    Human hobo sounds

--- gamemode/modules/hobo/sv_hobo.lua
+++ gamemode/modules/hobo/sv_hobo.lua
@@ -1,4 +1,4 @@
-local function MakeZombieSoundsAsHobo(ply)
+local function YellAsHobo(ply)
     if ply:EntIndex() == 0 then return end
     if not ply.nospamtime then
         ply.nospamtime = CurTime() - 2
@@ -7,13 +7,15 @@ local function MakeZombieSoundsAsHobo(ply)
         return
     end
     ply.nospamtime = CurTime()
-    local ran = math.random(1,3)
+    local ran = math.random(1,4)
     if ran == 1 then
-        ply:EmitSound("npc/zombie/zombie_voice_idle" .. tostring(math.random(1, 14)) .. ".wav", 100, 100)
+        ply:EmitSound("vo/npc/male01/gethellout.wav", 100, 100)
     elseif ran == 2 then
-        ply:EmitSound("npc/zombie/zombie_pain" .. tostring(math.random(1, 6)) .. ".wav", 100, 100)
+        ply:EmitSound("vo/npc/male01/no02.wav", 100, 100)
     elseif ran == 3 then
-        ply:EmitSound("npc/zombie/zombie_alert" .. tostring(math.random(1, 3)) .. ".wav", 100, 100)
+        ply:EmitSound("vo/npc/male01/watchout.wav", 100, 100)
+    elseif ran == 4 then
+        ply:EmitSound("vo/npc/male01/runforyourlife01.wav", 100, 100)
     end
 end
-concommand.Add("_hobo_emitsound", MakeZombieSoundsAsHobo)
+concommand.Add("_hobo_emitsound", YellAsHobo)
