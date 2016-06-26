081fa68c309dbec04f4ac0cf195b9fae1826fb9a
kiruthekiller <danila.shamaev@gmail.com>
2016-06-26 21:19:50 +0300

    Nice looking vote VGUI
    Now 20% cooler than old!

--- gamemode/modules/voting/cl_voting.lua
+++ gamemode/modules/voting/cl_voting.lua
@@ -1,234 +1,242 @@
-local QuestionVGUI = {}
-local PanelNum = 0
-local VoteVGUI = {}
-local function MsgDoVote(msg)
-    local _, chatY = chat.GetChatBoxPos()
+local tbl = {}
+local tbl2 = {}
+local next_keydown
 
-    local question = msg:ReadString()
-    local voteid = msg:ReadShort()
-    local timeleft = msg:ReadFloat()
-    if timeleft == 0 then
-        timeleft = 100
-    end
-    local OldTime = CurTime()
-    if not IsValid(LocalPlayer()) then return end -- Sent right before player initialisation
+local surface = surface
+local draw = draw
 
-    LocalPlayer():EmitSound("Town.d1_town_02_elevbell1", 100, 100)
-    local panel = vgui.Create("DFrame")
-    panel:SetPos(3 + PanelNum, chatY - 145)
-    panel:SetTitle(DarkRP.getPhrase("vote"))
-    panel:SetSize(140, 140)
-    panel:SetSizable(false)
-    panel.btnClose:SetVisible(false)
-    panel.btnMaxim:SetVisible(false)
-    panel.btnMinim:SetVisible(false)
-    panel:SetDraggable(false)
-    function panel:Close()
-        PanelNum = PanelNum - 140
-        VoteVGUI[voteid .. "vote"] = nil
+usermessage.Hook("DoVote", function(msg)
+	local question = msg:ReadString()
+	local voteid = msg:ReadShort()
+	local timeleft = msg:ReadFloat()
+	
+	local last = CurTime()
+		
+	LocalPlayer():EmitSound('Town.d1_town_02_elevbell1',100,100)
+		
+	local menu = vgui.Create("DPanel")
+	menu:SetSize(380,54-15)
+	menu:SetPos(-380,10+#tbl*(60-15))
+	menu:MoveTo(10,10+#tbl*(60-15),0.2)
+	menu.Close = function()
+		if !menu.cls then
+			menu.cls = true
+			timer.Simple(0.2,function()
+				menu:Remove()
+			end)
+			if #tbl>1 then
+				local i=0
+				local id = 0
+				for v,k in pairs(tbl) do
+					if k!=menu then
+						i=i+1
+						k:MoveTo(10,i*(60-15)-50+15,0.2)
+					else
+						menu:MoveTo(-380,v*(60-15)-50+15,0.2)
+						id=v
+					end
+				end
+				table.remove(tbl,id)
+				tbl[1].fff = true
+			else
+				menu:MoveTo(-380,10,0.2)
+				table.remove(tbl,1)
+			end
+		end
+	end
+	menu.Paint = function(self,w,h)
+		local time = timeleft-CurTime()+last
+		if time<=0 then menu:Close() end
+		surface.SetDrawColor(30,30,30,220)
+		surface.DrawRect(0,0,w,h)
+		
+		surface.SetDrawColor(0,0,0)
+		surface.DrawOutlinedRect(0,0,w,h)
+		
+		surface.SetDrawColor(127,160,255)
+		surface.DrawRect(0,0,w,5)
+		
+		surface.SetDrawColor(50,255,0)
+		surface.DrawRect(0,0,time/timeleft*w,5)
+		
+	end
+	menu.Think = function()
+		if menu.fff then
+			local f7 = input.IsKeyDown(KEY_F7)
+			local f8 = input.IsKeyDown(KEY_F8)
+			if f7 and !next_keydown then
+				LocalPlayer():ConCommand("vote " .. voteid .. " yea\n")	
+				menu:Close()
+				next_keydown=true
+			elseif f8 and !next_keydown  then
+				LocalPlayer():ConCommand("vote " .. voteid .. " nay\n")
+				menu:Close()
+				next_keydown=true
+			end
+			if !f7 and !f8 then
+				next_keydown=false	
+			end
+		end
+	end
+	
+	local label = vgui.Create("DLabel",menu)
+	label:SetPos(5, 25-15)
+	label:SetText(question)
+	label:SizeToContents()
+	
+	local no = vgui.Create("DButton",menu)
+	no:SetSize(42,20)
+	no:SetPos(333,25-15)
+	no:SetText("")
+	no.Paint = function(self,w,h)
+		surface.SetDrawColor(255,30,30)
+		surface.DrawRect(0,0,w,h)
+		draw.SimpleText(menu.fff and "No(F8)" or "Нет","DermaDefault",w/2,h/2,Color(0,0,0),TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER)
+		
+		surface.SetDrawColor(0,0,0)
+		surface.DrawOutlinedRect(0,0,w,h)
+	end
+	no.DoClick = function()
+		LocalPlayer():ConCommand("vote " .. voteid .. " nay\n")
+		menu:Close()
+	end
+	
+	local yes = vgui.Create("DButton",menu)
+	yes:SetSize(42,20)
+	yes:SetPos(286,25-15)
+	yes:SetText("")
+	yes.Paint = function(self,w,h)
+		surface.SetDrawColor(30,255,30)
+		surface.DrawRect(0,0,w,h)
+		draw.SimpleText(menu.fff and "Yes(F7)" or "Да","DermaDefault",w/2,h/2,Color(0,0,0),TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER)
+		
+		surface.SetDrawColor(0,0,0)
+		surface.DrawOutlinedRect(0,0,w,h)
+	end
+	yes.DoClick = function()
+		LocalPlayer():ConCommand("vote " .. voteid .. " yea\n")
+		menu:Close()
+	end
+	
+	if #tbl==0 then menu.fff=true end
+	table.insert(tbl,menu)
+	tbl2[voteid]=menu
+	
+end)
 
-        local num = 0
-        for k,v in SortedPairs(VoteVGUI) do
-            v:SetPos(num, ScrH() / 2 - 50)
-            num = num + 140
-        end
+usermessage.Hook("KillVoteVGUI",function(msg)
+	local id = msg:ReadShort()
+	if IsValid(tbl2[id]) then
+		tbl2[id]:Close()
+	end
+end)
 
-        for k,v in SortedPairs(QuestionVGUI) do
-            v:SetPos(num, ScrH() / 2 - 50)
-            num = num + 300
-        end
-        self:Remove()
-    end
 
-    function panel:Think()
-        self:SetTitle(DarkRP.getPhrase("time", math.Clamp(math.ceil(timeleft - (CurTime() - OldTime)), 0, 9999)))
-        if timeleft - (CurTime() - OldTime) <= 0 then
-            panel:Close()
-        end
-    end
+local qtbl = {}
+local qtbl2 = {}
 
-    panel:SetKeyboardInputEnabled(false)
-    panel:SetMouseInputEnabled(true)
-    panel:SetVisible(true)
+usermessage.Hook("DoQuestion", function(msg)
+	local question = msg:ReadString()
+	local quesid = msg:ReadString()
+	local timeleft = msg:ReadFloat()
+	
+	LocalPlayer():EmitSound('Town.d1_town_02_elevbell1',100,100)
+	
+	local last = CurTime()
+		
+	local menu = vgui.Create("DPanel")
+	menu:SetSize(380,70-15)
+	menu:SetPos(ScrW()+380,10+#qtbl*(80-15))
+	menu:MoveTo(ScrW()-390,10+#qtbl*(80-15),0.2)
+	menu.Close = function()
+		if !menu.cls then
+			menu.cls = true
+			timer.Simple(0.2,function()
+				menu:Remove()
+			end)
+			if #qtbl>1 then
+				local i=0
+				local id = 0
+				for v,k in pairs(qtbl) do
+					if k!=menu then
+						i=i+1
+						k:MoveTo(ScrW()-390,i*(80-15)-70+15,0.2)
+					else
+						menu:MoveTo(ScrW()+380,v*(80-15)-70+15,0.2)
+						id=v
+					end
+				end
+				table.remove(qtbl,id)
+			else
+				menu:MoveTo(ScrW()+380,10,0.2)
+				table.remove(qtbl,1)
+			end
+		end
+	end
+	menu.Paint = function(self,w,h)
+		local time = timeleft-CurTime()+last
+		if time<=0 then menu:Close() end
+		surface.SetDrawColor(30,30,30,220)
+		surface.DrawRect(0,0,w,h)
+		
+		surface.SetDrawColor(0,0,0)
+		surface.DrawOutlinedRect(0,0,w,h)
+		
+		surface.SetDrawColor(127,160,255)
+		surface.DrawRect(0,0,w,5)
+		
+		surface.SetDrawColor(50,255,0)
+		surface.DrawRect(0,0,time/timeleft*w,5)
+		
+	end
+	
+	local label = vgui.Create("DLabel",menu)
+	label:SetPos(5, 24-15)
+	label:SetText(question)
+	label:SizeToContents()
+	
+	local no = vgui.Create("DButton",menu)
+	no:SetSize(42,20)
+	no:SetPos(328,40-15)
+	no:SetText("")
+	no.Paint = function(self,w,h)
+		surface.SetDrawColor(255,30,30)
+		surface.DrawRect(0,0,w,h)
+		draw.SimpleText("No","DermaDefault",w/2,h/2,Color(0,0,0),TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER)
+		
+		surface.SetDrawColor(0,0,0)
+		surface.DrawOutlinedRect(0,0,w,h)
+	end
+	no.DoClick = function()
+		LocalPlayer():ConCommand("ans " .. quesid .. " 2\n")
+		menu:Close()
+	end
+	
+	local yes = vgui.Create("DButton",menu)
+	yes:SetSize(42,20)
+	yes:SetPos(281,40-15)
+	yes:SetText("")
+	yes.Paint = function(self,w,h)
+		surface.SetDrawColor(30,255,30)
+		surface.DrawRect(0,0,w,h)
+		draw.SimpleText("Yes","DermaDefault",w/2,h/2,Color(0,0,0),TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER)
+		
+		surface.SetDrawColor(0,0,0)
+		surface.DrawOutlinedRect(0,0,w,h)
+	end
+	yes.DoClick = function()
+		LocalPlayer():ConCommand("ans " .. quesid .. " 1\n")
+		menu:Close()
+	end
+	
+	table.insert(qtbl,menu)
+	qtbl2[quesid]=menu
+	
+end)
 
-    for i = 22, string.len(question), 22 do
-        if not string.find(string.sub(question, i - 20, i), "\n", 1, true) then
-            question = string.sub(question, 1, i) .. "\n" .. string.sub(question, i + 1, string.len(question))
-        end
-    end
-
-    local label = vgui.Create("DLabel")
-    label:SetParent(panel)
-    label:SetPos(5, 25)
-    label:SetText(DarkRP.deLocalise(question))
-    label:SizeToContents()
-    label:SetVisible(true)
-
-    local nextHeight = label:GetTall() > 78 and label:GetTall() - 78 or 0 -- Make panel taller for divider and buttons
-    panel:SetTall(panel:GetTall() + nextHeight)
-
-    local divider = vgui.Create("Divider")
-    divider:SetParent(panel)
-    divider:SetPos(2, panel:GetTall() - 30)
-    divider:SetSize(180, 2)
-    divider:SetVisible(true)
-
-    local ybutton = vgui.Create("Button")
-    ybutton:SetParent(panel)
-    ybutton:SetPos(25, panel:GetTall() - 25)
-    ybutton:SetSize(40, 20)
-    ybutton:SetCommand("!")
-    ybutton:SetText(DarkRP.getPhrase("yes"))
-    ybutton:SetVisible(true)
-    ybutton.DoClick = function()
-        LocalPlayer():ConCommand("vote " .. voteid .. " yea\n")
-        panel:Close()
-    end
-
-    local nbutton = vgui.Create("Button")
-    nbutton:SetParent(panel)
-    nbutton:SetPos(70, panel:GetTall() - 25)
-    nbutton:SetSize(40, 20)
-    nbutton:SetCommand("!")
-    nbutton:SetText(DarkRP.getPhrase("no"))
-    nbutton:SetVisible(true)
-    nbutton.DoClick = function()
-        LocalPlayer():ConCommand("vote " .. voteid .. " nay\n")
-        panel:Close()
-    end
-
-    PanelNum = PanelNum + 140
-    VoteVGUI[voteid .. "vote"] = panel
-    panel:SetSkin(GAMEMODE.Config.DarkRPSkin)
-end
-usermessage.Hook("DoVote", MsgDoVote)
-
-local function KillVoteVGUI(msg)
-    local id = msg:ReadShort()
-
-    if VoteVGUI[id .. "vote"] and VoteVGUI[id .. "vote"]:IsValid() then
-        VoteVGUI[id .. "vote"]:Close()
-    end
-end
-usermessage.Hook("KillVoteVGUI", KillVoteVGUI)
-
-local function MsgDoQuestion(msg)
-    local question = msg:ReadString()
-    local quesid = msg:ReadString()
-    local timeleft = msg:ReadFloat()
-    if timeleft == 0 then
-        timeleft = 100
-    end
-    local OldTime = CurTime()
-    LocalPlayer():EmitSound("Town.d1_town_02_elevbell1", 100, 100)
-    local panel = vgui.Create("DFrame")
-    panel:SetPos(3 + PanelNum, ScrH() / 2 - 50) -- Times 140 because if the question is the second screen, the first screen is always a vote screen.
-    panel:SetSize(300, 140)
-    panel:SetSizable(false)
-    panel.btnClose:SetVisible(false)
-    panel.btnMaxim:SetVisible(false)
-    panel.btnMinim:SetVisible(false)
-    panel:SetKeyboardInputEnabled(false)
-    panel:SetMouseInputEnabled(true)
-    panel:SetVisible(true)
-
-    function panel:Close()
-        PanelNum = PanelNum - 300
-        QuestionVGUI[quesid .. "ques"] = nil
-        local num = 0
-        for k,v in SortedPairs(VoteVGUI) do
-            v:SetPos(num, ScrH() / 2 - 50)
-            num = num + 140
-        end
-
-        for k,v in SortedPairs(QuestionVGUI) do
-            v:SetPos(num, ScrH() / 2 - 50)
-            num = num + 300
-        end
-
-        self:Remove()
-    end
-
-    function panel:Think()
-        self:SetTitle(DarkRP.getPhrase("time", math.Clamp(math.ceil(timeleft - (CurTime() - OldTime)), 0, 9999)))
-        if timeleft - (CurTime() - OldTime) <= 0 then
-            panel:Close()
-        end
-    end
-
-    local label = vgui.Create("DLabel")
-    label:SetParent(panel)
-    label:SetPos(5, 30)
-    label:SetSize(380, 40)
-    label:SetText(DarkRP.deLocalise(question))
-    label:SetVisible(true)
-
-    local divider = vgui.Create("Divider")
-    divider:SetParent(panel)
-    divider:SetPos(2, 80)
-    divider:SetSize(380, 2)
-    divider:SetVisible(true)
-
-    local ybutton = vgui.Create("DButton")
-    ybutton:SetParent(panel)
-    ybutton:SetPos(105, 100)
-    ybutton:SetSize(40, 20)
-    ybutton:SetText(DarkRP.getPhrase("yes"))
-    ybutton:SetVisible(true)
-    ybutton.DoClick = function()
-        LocalPlayer():ConCommand("ans " .. quesid .. " 1\n")
-        panel:Close()
-    end
-
-    local nbutton = vgui.Create("DButton")
-    nbutton:SetParent(panel)
-    nbutton:SetPos(155, 100)
-    nbutton:SetSize(40, 20)
-    nbutton:SetText(DarkRP.getPhrase("no"))
-    nbutton:SetVisible(true)
-    nbutton.DoClick = function()
-        LocalPlayer():ConCommand("ans " .. quesid .. " 2\n")
-        panel:Close()
-    end
-
-    PanelNum = PanelNum + 300
-    QuestionVGUI[quesid .. "ques"] = panel
-
-    panel:SetSkin(GAMEMODE.Config.DarkRPSkin)
-end
-usermessage.Hook("DoQuestion", MsgDoQuestion)
-
-local function KillQuestionVGUI(msg)
-    local id = msg:ReadString()
-
-    if QuestionVGUI[id .. "ques"] and QuestionVGUI[id .. "ques"]:IsValid() then
-        QuestionVGUI[id .. "ques"]:Close()
-    end
-end
-usermessage.Hook("KillQuestionVGUI", KillQuestionVGUI)
-
-local function DoVoteAnswerQuestion(ply, cmd, args)
-    if not args[1] then return end
-
-    local vote = 0
-    if tonumber(args[1]) == 1 or string.lower(args[1]) == "yes" or string.lower(args[1]) == "true" then vote = 1 end
-
-    for k,v in pairs(VoteVGUI) do
-        if IsValid(v) then
-            local ID = string.sub(k, 1, -5)
-            VoteVGUI[k]:Close()
-            RunConsoleCommand("vote", ID, vote)
-            return
-        end
-    end
-
-    for k,v in pairs(QuestionVGUI) do
-        if IsValid(v) then
-            local ID = string.sub(k, 1, -5)
-            QuestionVGUI[k]:Close()
-            RunConsoleCommand("ans", ID, vote)
-            return
-        end
-    end
-end
-concommand.Add("rp_vote", DoVoteAnswerQuestion)
+usermessage.Hook("KillQuestionVGUI",function(msg)
+	local id = msg:ReadShort()
+	if IsValid(qtbl2[id]) then
+		qtbl2[id]:Close()
+	end
+end)
