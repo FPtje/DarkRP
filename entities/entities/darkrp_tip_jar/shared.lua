851dce3a7180bdf5eff997385f65bc673a4e5240
FPtje <fpeijnenburg@gmail.com>
2017-02-18 10:33:55 +0100

    Added allowTools option for entities. Defaults to false.
    Allows people to use any tool on their entities (as long as prop protections
    don't disagree)

--- entities/entities/darkrp_tip_jar/shared.lua
+++ entities/entities/darkrp_tip_jar/shared.lua
@@ -77,7 +77,3 @@ end
 function ENT:ClearDonations()
     table.Empty(self.madeDonations)
 end
-
-function ENT:CanTool(ply, trace, tool)
-    if tool == "remover" and ply == self:Getowning_ent() then return true end
-end
