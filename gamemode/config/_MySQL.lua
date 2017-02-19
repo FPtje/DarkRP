c0d0089a32a808e426f4bdc73bf966a85b70303d
EnigmaticOctopus <mynameisthenekit@gmail.com>
2017-02-19 15:04:05 +0200

    Update link to mysql lib
    Because previous thread is closed.

--- gamemode/config/_MySQL.lua
+++ gamemode/config/_MySQL.lua
@@ -22,7 +22,7 @@ RP_MySQLConfig.MultiStatements = false -- Only available in tmysql4: allow multi
 MANUAL!
 HOW TO USE MySQL FOR DARKRP!
 Download tmysql4 and read the guide here:
-https://facepunch.com/showthread.php?t=1442438
+https://facepunch.com/showthread.php?t=1515853
 
 
 WHAT TO DO IF YOU CAN'T GET IT TO WORK!
