18ac640799be179b6c3ce63a6ab7668fe434f83a
Falco Peijnenburg <fpeijnenburg@gmail.com>
2018-07-14 10:25:33 +0200

    Fixed entity limits on non-standard F4 menu entities when pocketing
    Fixes #2774

--- gamemode/modules/base/sv_gamemode_functions.lua
+++ gamemode/modules/base/sv_gamemode_functions.lua
@@ -227,7 +227,7 @@ function GM:EntityRemoved(ent)
     end
 
     local owner = ent.Getowning_ent and ent:Getowning_ent() or Player(ent.SID or 0)
-    if ent.DarkRPItem and IsValid(owner) then owner:removeCustomEntity(ent.DarkRPItem) end
+    if ent.DarkRPItem and IsValid(owner) and not ent.IsPocketing then owner:removeCustomEntity(ent.DarkRPItem) end
     if ent.isKeysOwnable and ent:isKeysOwnable() then ent:removeDoorData() end
 end
 
