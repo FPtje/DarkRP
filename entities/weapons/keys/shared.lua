42d0196217451a4fedc9f225fb01afabbae8654c
Pierre Fichepoil <ExtReMLapin@users.noreply.github.com>
2018-08-11 14:00:02 +0200

    typo, extra ")" removed
    woops

--- entities/weapons/keys/shared.lua
+++ entities/weapons/keys/shared.lua
@@ -136,7 +136,7 @@ end
 
 function SWEP:Reload()
     local trace = self:GetOwner():GetEyeTrace()
-    if not IsValid(trace.Entity) or ((not trace.Entity:isDoor() and not trace.Entity:IsVehicle()) or self.Owner:EyePos():DistToSqr(trace.HitPos) > 40000)) then
+    if not IsValid(trace.Entity) or ((not trace.Entity:isDoor() and not trace.Entity:IsVehicle()) or self.Owner:EyePos():DistToSqr(trace.HitPos) > 40000) then
         if CLIENT and not DarkRP.disabledDefaults["modules"]["animations"] then RunConsoleCommand("_DarkRP_AnimationMenu") end
         return
     end
