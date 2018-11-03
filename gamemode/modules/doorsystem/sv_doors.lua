7feb1a95ff78a9a8d75213f19e6ed6c2fe2ad1a8
Falco Peijnenburg <fpeijnenburg@gmail.com>
2018-11-03 13:58:53 +0100

    unownalldoors command: Use playerSellX and getXCost hooks
    Also optimised to not loop through all entities
    
    Credits: @mcNuggets1 in #2080

--- gamemode/modules/doorsystem/sv_doors.lua
+++ gamemode/modules/doorsystem/sv_doors.lua
@@ -393,18 +393,46 @@ DarkRP.defineChatCommand("toggleown", OwnDoor)
 
 local function UnOwnAll(ply, cmd, args)
     local amount = 0
-    for _, v in ipairs(ents.GetAll()) do
-        if v:isKeysOwnedBy(ply) and v:isDoor() and not IsValid(v.EntOwner) then -- EntOwner is from SCARs
-            amount = amount + 1
-            v:Fire("unlock", "", 0)
-            v:keysUnOwn(ply)
-            local cost = (v:IsVehicle() and GAMEMODE.Config.vehiclecost or GAMEMODE.Config.doorcost) * 2 / 3 + 0.5
-            ply:addMoney(math.floor(cost))
-            ply.Ownedz[v:EntIndex()] = nil
+    local cost = 0
+
+    local unownables = {}
+    for entIndex, ent in pairs(ply.Ownedz or {}) do
+        if not ent:isKeysOwnable() then ply.Ownedz[entIndex] = nil continue end
+        table.insert(unownables, ent)
+    end
+
+    for _, otherPly in ipairs(player.GetAll()) do
+        if ply == otherPly then continue end
+
+        for _, ent in pairs(otherPly.Ownedz or {}) do
+            if IsValid(ent) and ent:isKeysOwnedBy(ply) then
+                table.insert(unownables, ent)
+            end
         end
     end
-    ply.OwnedNumz = 0
-    DarkRP.notify(ply, 2, 4, DarkRP.getPhrase("sold_x_doors", amount,DarkRP.formatMoney(amount * math.floor((GAMEMODE.Config.doorcost * 0.66666666666666) + 0.5))))
+
+    for entIndex, ent in pairs(unownables) do
+        local bAllowed, _strReason = hook.Call("playerSell" .. (ent:IsVehicle() and "Vehicle" or "Door"), GAMEMODE, ply, ent)
+
+        if bAllowed == false then continue end
+
+        if ent:isMasterOwner(ply) then
+            ent:Fire("unlock", "", 0)
+        end
+
+        ent:keysUnOwn(ply)
+        amount = amount + 1
+
+        local GiveMoneyBack = math.floor((hook.Call("get" .. (ent:IsVehicle() and "Vehicle" or "Door") .. "Cost", GAMEMODE, ply, ent) * 0.666) + 0.5)
+        hook.Call("playerKeysSold", GAMEMODE, ply, ent, GiveMoneyBack)
+        cost = cost + GiveMoneyBack
+    end
+
+    if amount == 0 then return "" end
+
+    ply:addMoney(math.floor(cost))
+
+    DarkRP.notify(ply, 2, 4, DarkRP.getPhrase("sold_x_doors", amount, DarkRP.formatMoney(math.floor(cost))))
     return ""
 end
 DarkRP.defineChatCommand("unownalldoors", UnOwnAll)
