341a0384e1767c04f326103747b5a20a4165bc61
Ilya Finogenov <bywgis@gmail.com>
2019-04-26 14:07:41 +0000

    Update cl_init.lua

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -16,13 +16,8 @@ local function LoadModules()
     for _, folder in SortedPairs(folders, true) do
         if DarkRP.disabledDefaults["modules"][folder] then continue end
 
-        for _, File in SortedPairs(file.Find(root .. folder .. "/sh_*.lua", "LUA"), true) do
-            if File == "sh_interface.lua" then continue end
-            include(root .. folder .. "/" .. File)
-        end
-
-        for _, File in SortedPairs(file.Find(root .. folder .. "/cl_*.lua", "LUA"), true) do
-            if File == "cl_interface.lua" then continue end
+        for _, File in SortedPairs(file.Find(root .. folder .. "/*.lua", "LUA"), true) do
+            if string.find( File:lower(), "_interface" ) then continue end
             include(root .. folder .. "/" .. File)
         end
     end
