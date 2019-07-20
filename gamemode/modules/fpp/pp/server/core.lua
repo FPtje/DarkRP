be6f9f17f10e32422613deb56259ab9dd0dd3822
Falco Peijnenburg <fpeijnenburg@gmail.com>
2019-07-20 13:48:22 +0200

    Merged FPP

--- gamemode/modules/fpp/pp/server/core.lua
+++ gamemode/modules/fpp/pp/server/core.lua
@@ -118,6 +118,10 @@ if undo then
     end
 end
 
+hook.Add("PlayerSpawnedSWEP", "FPP.Spawn.SWEP", function(ply, ent)
+    ent:CPPISetOwner(ply)
+end)
+
 --------------------------------------------------------------------------------------
 --The protecting itself
 --------------------------------------------------------------------------------------
