b9d6baa0d6389e76cba887bc60f7d92299776661
crester <35332822+cresterienvogel@users.noreply.github.com>
2021-01-02 02:40:05 +0700

    a slightly smarter solution

--- gamemode/modules/hobo/sv_hobo.lua
+++ gamemode/modules/hobo/sv_hobo.lua
@@ -1,21 +1,37 @@
-local function MakeZombieSoundsAsHobo(ply)
-    if ply:EntIndex() == 0 then return end
-    if not ply.nospamtime then
-        ply.nospamtime = CurTime() - 2
-    end
+local sounds = {
+	[1] = {
+		range = 14,
+		path = "npc/zombie/zombie_voice_idle"
+	},
+	[2] = {
+		range = 6,
+		path = "npc/zombie/zombie_pain"
+	},
+	[3] = {
+		range = 3,
+		path = "npc/zombie/zombie_alert"
+	}
+}
 
-    local Team = ply:Team()
-    if not RPExtraTeams[Team] or not RPExtraTeams[Team].hobo or CurTime() < (ply.nospamtime + 1.3) or (ply:GetActiveWeapon():IsValid() and ply:GetActiveWeapon():GetClass() ~= "weapon_bugbait") then
-        return
-    end
-    ply.nospamtime = CurTime()
-    local ran = math.random(1,3)
-    if ran == 1 then
-        ply:EmitSound("npc/zombie/zombie_voice_idle" .. math.random(1, 14) .. ".wav", 100, 100)
-    elseif ran == 2 then
-        ply:EmitSound("npc/zombie/zombie_pain" .. math.random(1, 6) .. ".wav", 100, 100)
-    elseif ran == 3 then
-        ply:EmitSound("npc/zombie/zombie_alert" .. math.random(1, 3) .. ".wav", 100, 100)
-    end
+local function MakeZombieSoundsAsHobo(pl)
+	if pl:EntIndex() == 0 then 
+		return 
+	end
+
+	if not pl.nospamtime then
+		pl.nospamtime = CurTime() - 2
+	end
+
+	local team = pl:Team()
+	if not RPExtrateams[team] or not RPExtrateams[team].hobo or CurTime() < (pl.nospamtime + 1.3) or (pl:GetActiveWeapon():IsValid() and pl:GetActiveWeapon():GetClass() ~= "weapon_bugbait") then
+		return
+	end
+
+	pl.nospamtime = CurTime()
+
+	local ran = math.random(1, 3)
+	local sound = sounds[ran]
+	pl:EmitSound(sound.path .. math.random(1, sound.range) .. ".wav", 100, 100)
 end
+
 concommand.Add("_hobo_emitsound", MakeZombieSoundsAsHobo)
