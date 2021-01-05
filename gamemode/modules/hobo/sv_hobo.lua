a085eea4626ca80e0369a0ce1c77c751b5f6aa8a
Falco Peijnenburg <falco.peijnenburg@channable.com>
2021-01-05 21:24:20 +0100

    Style fixes for #3078

--- gamemode/modules/hobo/sv_hobo.lua
+++ gamemode/modules/hobo/sv_hobo.lua
@@ -14,24 +14,23 @@ local sounds = {
 }
 
 local function MakeZombieSoundsAsHobo(ply)
-    if ply:EntIndex() == 0 then 
-        return 
+    if ply:EntIndex() == 0 then
+        return
     end
 
     if not ply.nospamtime then
         ply.nospamtime = CurTime() - 2
     end
 
-    local team = ply:Team()
-    if not RPExtrateams[team] or not RPExtrateams[team].hobo or CurTime() < (ply.nospamtime + 1.3) or (ply:GetActiveWeapon():IsValid() and ply:GetActiveWeapon():GetClass() ~= "weapon_bugbait") then
+    local t = ply:Team()
+    if not RPExtrateams[t] or not RPExtrateams[t].hobo or CurTime() < (ply.nospamtime + 1.3) or (ply:GetActiveWeapon():IsValid() and ply:GetActiveWeapon():GetClass() ~= "weapon_bugbait") then
         return
     end
 
     ply.nospamtime = CurTime()
 
     local ran = math.random(1, 3)
-    local sound = sounds[ran]
-    ply:EmitSound(sound.path .. math.random(1, sound.range) .. ".wav", 100, 100)
+    local snd = sounds[ran]
+    ply:EmitSound(snd.path .. math.random(1, snd.range) .. ".wav", 100, 100)
 end
-
 concommand.Add("_hobo_emitsound", MakeZombieSoundsAsHobo)
