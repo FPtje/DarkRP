2c8fb704ebb9f6c5c070855dd4d71978773a3704
crester <35332822+cresterienvogel@users.noreply.github.com>
2021-01-05 17:48:11 +0700

    pl to ply
    by the florian's request

--- gamemode/modules/hobo/sv_hobo.lua
+++ gamemode/modules/hobo/sv_hobo.lua
@@ -13,25 +13,25 @@ local sounds = {
     }
 }
 
-local function MakeZombieSoundsAsHobo(pl)
-    if pl:EntIndex() == 0 then 
+local function MakeZombieSoundsAsHobo(ply)
+    if ply:EntIndex() == 0 then 
         return 
     end
 
-    if not pl.nospamtime then
-        pl.nospamtime = CurTime() - 2
+    if not ply.nospamtime then
+        ply.nospamtime = CurTime() - 2
     end
 
-    local team = pl:Team()
-    if not RPExtrateams[team] or not RPExtrateams[team].hobo or CurTime() < (pl.nospamtime + 1.3) or (pl:GetActiveWeapon():IsValid() and pl:GetActiveWeapon():GetClass() ~= "weapon_bugbait") then
+    local team = ply:Team()
+    if not RPExtrateams[team] or not RPExtrateams[team].hobo or CurTime() < (ply.nospamtime + 1.3) or (ply:GetActiveWeapon():IsValid() and ply:GetActiveWeapon():GetClass() ~= "weapon_bugbait") then
         return
     end
 
-    pl.nospamtime = CurTime()
+    ply.nospamtime = CurTime()
 
     local ran = math.random(1, 3)
     local sound = sounds[ran]
-    pl:EmitSound(sound.path .. math.random(1, sound.range) .. ".wav", 100, 100)
+    ply:EmitSound(sound.path .. math.random(1, sound.range) .. ".wav", 100, 100)
 end
 
 concommand.Add("_hobo_emitsound", MakeZombieSoundsAsHobo)
