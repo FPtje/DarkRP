abd46b59829e90f2129357fd8c12d8a969efc1bc
Falco Peijnenburg <fpeijnenburg@gmail.com>
2021-01-31 19:47:08 +0100

    Remove unnecessary string.format in starting lottery

--- gamemode/modules/police/sv_commands.lua
+++ gamemode/modules/police/sv_commands.lua
@@ -115,7 +115,7 @@ local function DoLottery(ply, amount)
 
     amount = tonumber(amount)
     if not amount then
-        DarkRP.notify(ply, 1, 5, string.format(DarkRP.getPhrase("lottery_please_specify_an_entry_cost", DarkRP.formatMoney(GAMEMODE.Config.minlotterycost), DarkRP.formatMoney(GAMEMODE.Config.maxlotterycost) )))
+        DarkRP.notify(ply, 1, 5, DarkRP.getPhrase("lottery_please_specify_an_entry_cost", DarkRP.formatMoney(GAMEMODE.Config.minlotterycost), DarkRP.formatMoney(GAMEMODE.Config.maxlotterycost)))
         return ""
     end
 
