5ad6da1f73edf6de7b24107f537bc3312ff02b40
David <mcNuggets@outlook.de>
2021-05-18 14:55:06 +0200

    Rename food sound var

--- entities/entities/spawned_food/shared.lua
+++ entities/entities/spawned_food/shared.lua
@@ -4,7 +4,7 @@ ENT.PrintName = "Spawned Food"
 ENT.Author = "Rickster"
 ENT.Spawnable = false
 ENT.IsSpawnedFood = true
-ENT.UseSound = "vo/sandwicheat09.mp3" -- Requires Team Fortress 2
+ENT.EatSound = "vo/sandwicheat09.mp3" -- Requires Team Fortress 2
 
 function ENT:SetupDataTables()
     self:NetworkVar("Entity", 1, "owning_ent")
