0d9b58a213c49273ff0c3fe24754f167f16f5fa5
George Gruber <jbogusevs@gmail.com>
2021-10-04 13:29:40 +0100

    Fixed indentation

--- entities/entities/spawned_shipment/init.lua
+++ entities/entities/spawned_shipment/init.lua
@@ -92,8 +92,8 @@ function ENT:Use(activator, caller)
 
     local canUse, reason = hook.Call("canDarkRPUse", nil, activator, self, caller)
     if canUse == false then
-      if reason then DarkRP.notify(activator, 1, 4, reason) end
-      return
+        if reason then DarkRP.notify(activator, 1, 4, reason) end
+        return
     end
 
     hook.Call("playerOpenedShipment", nil, activator, self)
