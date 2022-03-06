43b55d9e7359fc6cce0c6c953e58dae1455635a4
Falco Peijnenburg <falco.peijnenburg@channable.com>
2022-03-06 13:00:09 +0100

    Revert change to FPP

--- gamemode/modules/fpp/pp/client/hud.lua
+++ gamemode/modules/fpp/pp/client/hud.lua
@@ -5,9 +5,7 @@ surface.CreateFont("TabLarge", {
     weight = 700,
     antialias = true,
     shadow = false,
-    font = "Trebuchet MS",
-    extended = true,
-})
+    font = "Trebuchet MS"})
 
 hook.Add("CanTool", "FPP_CL_CanTool", function(ply, trace, tool) -- Prevent client from SEEING his toolgun shoot while it doesn't shoot serverside.
     if IsValid(trace.Entity) and not FPP.canTouchEnt(trace.Entity, "Toolgun") then
