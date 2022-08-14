993410551d1b8b9241215460208242d92d19f33c
Dark Fated <49955245+darkfated@users.noreply.github.com>
2022-08-14 15:01:58 +0300

    Fixed overlapping of boxes

--- gamemode/modules/chat/cl_chatlisteners.lua
+++ gamemode/modules/chat/cl_chatlisteners.lua
@@ -45,7 +45,7 @@ local function drawChatReceivers()
     if not receivers then return end
 
     local x, y = chat.GetChatBoxPos()
-    y = y - 21
+    y = y - 25
 
     local receiversCount = #receivers
     -- No one hears you
@@ -58,7 +58,7 @@ local function drawChatReceivers()
         return
     end
 
-    draw.WordBox(2, x, y - (receiversCount * 21), DarkRP.getPhrase("hear_certain_persons", currentConfig.text), "DarkRPHUD1", Color(0,0,0,160), Color(0,255,0,255))
+    draw.WordBox(2, x, y - (receiversCount * 25), DarkRP.getPhrase("hear_certain_persons", currentConfig.text), "DarkRPHUD1", Color(0,0,0,160), Color(0,255,0,255))
     for i = 1, receiversCount, 1 do
         if not IsValid(receivers[i]) then
             receivers[i] = receivers[#receivers]
@@ -66,7 +66,7 @@ local function drawChatReceivers()
             continue
         end
 
-        draw.WordBox(2, x, y - (i - 1) * 21, receivers[i]:Nick(), "DarkRPHUD1", Color(0, 0, 0, 160), color_white)
+        draw.WordBox(2, x, y - (i - 1) * 25, receivers[i]:Nick(), "DarkRPHUD1", Color(0, 0, 0, 160), color_white)
     end
 end
 
